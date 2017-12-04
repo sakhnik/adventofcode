da83641858c0877518d9c7ad0b650627da2ef279
Anatolii Sakhnik <sakhnik@gmail.com>
2017-12-04 07:23:06 +0200

    Finish day 4

--- 4.cpp
+++ 4.cpp
@@ -2,7 +2,9 @@
 #include <sstream>
 #include <cassert>
 #include <unordered_set>
+#include <algorithm>
 
+template <typename FilterT>
 unsigned Count(const std::string &input)
 {
 	unsigned count{0};
@@ -10,13 +12,13 @@ unsigned Count(const std::string &input)
 	std::string line;
 	while (std::getline(iss, line))
 	{
-		std::unordered_set<std::string> words;
+		FilterT filter;
 		std::istringstream is(line);
 		std::string word;
 		bool isBad{false};
 		while (is >> word && !word.empty() && !isBad)
 		{
-			isBad = !words.insert(word).second;
+			isBad = filter.Add(word);
 		}
 
 		if (!isBad)
@@ -25,13 +27,37 @@ unsigned Count(const std::string &input)
 	return count;
 }
 
+struct ExactFilter
+{
+	bool Add(const std::string &word)
+	{
+		return !_words.insert(word).second;
+	}
+
+private:
+	std::unordered_set<std::string> _words;
+};
+
+struct AnagramFilter
+{
+	bool Add(std::string word)
+	{
+		std::sort(word.begin(), word.end());
+		return !_words.insert(word).second;
+	}
+
+private:
+	std::unordered_set<std::string> _words;
+};
+
 int main()
 {
-	assert(Count("aa bb cc dd ee") == 1);
-	assert(Count("aa bb cc dd aa") == 0);
-	assert(Count("aa bb cc dd aaa") == 1);
+	assert(Count<ExactFilter>("aa bb cc dd ee") == 1);
+	assert(Count<ExactFilter>("aa bb cc dd aa") == 0);
+	assert(Count<ExactFilter>("aa bb cc dd aaa") == 1);
 
 	std::string input((std::istreambuf_iterator<char>(std::cin)),
 					  std::istreambuf_iterator<char>());
-	std::cout << Count(input) << std::endl;
+	std::cout << Count<ExactFilter>(input) << std::endl;
+	std::cout << Count<AnagramFilter>(input) << std::endl;
 }
