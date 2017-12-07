70a5d0498c49688cff0deeb086cac7440e889876
Anatolii Sakhnik <sakhnik@gmail.com>
2017-12-07 08:56:20 +0200

    Day 7-2

--- 7.cpp
+++ 7.cpp
@@ -2,11 +2,15 @@
 #include <unordered_map>
 #include <sstream>
 #include <cassert>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
 struct Data
 {
-	unsigned weight;
+	unsigned weight = 0;
 	std::string parent;
+	std::vector<std::string> children;
 };
 
 typedef std::unordered_map<std::string, Data> TreeT;
@@ -30,6 +34,7 @@ TreeT ParseInput(std::istream &is)
 			if (child.back() == ',')
 				child.pop_back();
 			tree[child].parent = name;
+			tree[name].children.push_back(child);
 		}
 	}
 
@@ -49,6 +54,40 @@ std::string FindRoot(const TreeT &tree)
 	return it->first;
 }
 
+// total
+unsigned CheckWeight(const TreeT &tree, const std::string &node)
+{
+	auto it = tree.find(node);
+	if (it->second.children.empty())
+		return it->second.weight;
+
+	std::vector<unsigned> weight;
+	for (const auto &child : it->second.children)
+		weight.push_back(CheckWeight(tree, child));
+
+	// calculate median
+	std::vector<unsigned> weight2(weight);
+	auto it_med = weight2.begin() + weight2.size() / 2;
+	std::nth_element(weight2.begin(), it_med, weight2.end());
+	auto median = *it_med;
+
+	unsigned total = std::accumulate(weight.begin(), weight.end(), 0);
+	auto j = std::find_if(weight.begin(), weight.end(), [=](unsigned w) { return w != median; });
+	if (j != weight.end())
+	{
+		unsigned idx = j - weight.begin();
+		auto it_ch = tree.find(it->second.children[idx]);
+		throw median - weight[idx] + it_ch->second.weight;
+	}
+
+	std::sort(weight.begin(), weight.end());
+	unsigned expect = weight[weight.size()/2];
+	if (weight.front() != expect || weight.back() != expect)
+		throw expect;
+
+	return it->second.weight + total;
+}
+
 int main()
 {
 	const char *const test =
@@ -66,8 +105,27 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57))";
 
-	assert(FindRoot(ParseInput(std::istringstream(test))) == "tknk");
+	auto testTree = ParseInput(std::istringstream(test));
+	assert(FindRoot(testTree) == "tknk");
+	try
+	{
+		CheckWeight(testTree, FindRoot(testTree));
+		assert(!"Should get here");
+	}
+	catch (unsigned res)
+	{
+		assert(res == 60);
+	}
 
 	auto tree = ParseInput(std::cin);
-	std::cout << FindRoot(tree) << std::endl;
+	auto root = FindRoot(tree);
+	std::cout << root << std::endl;
+	try
+	{
+		CheckWeight(tree, root);
+	}
+	catch (unsigned res)
+	{
+		std::cout << res << std::endl;
+	}
 }
