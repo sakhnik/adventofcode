782539695e8f18db419ad7ad5e64b47a278b1cd8
Anatolii Sakhnik <sakhnik@gmail.com>
2017-12-11 08:56:13 +0200

    Day 11-2

--- 11.cpp
+++ 11.cpp
@@ -3,16 +3,22 @@
 #include <iostream>
 #include <fstream>
 
-unsigned Count(const char *path)
+std::pair<unsigned, unsigned> Count(const char *path)
 {
 	// A good guide to hexagonal grids is here:
 	// https://www.redblobgames.com/grids/hexagons/
 	// We'll use cube coordinates (a diagonal slice of cube is hex grid too)
 
 	int x{0}, y{0}, z{0};
+	auto calc_dist = [&] { return ((x < 0 ? -x : x) + (y < 0 ? -y : y) + (z < 0 ? -z : z)) / 2; };
+	unsigned max_dist{0};
 
 	while (char ch = *path++)
 	{
+		unsigned dist = calc_dist();
+		if (dist > max_dist)
+			max_dist = dist;
+
 		switch (ch)
 		{
 		case 'n':
@@ -58,20 +64,22 @@ unsigned Count(const char *path)
 		}
 	}
 
-	return ((x < 0 ? -x : x) + (y < 0 ? -y : y) + (z < 0 ? -z : z)) / 2;
+	return { calc_dist(), max_dist };
 }
 
 TEST_CASE("main")
 {
-	REQUIRE(Count("ne,ne,ne,") == 3);
-	REQUIRE(Count("ne,ne,sw,sw,") == 0);
-	REQUIRE(Count("ne,ne,s,s,") == 2);
-	REQUIRE(Count("se,sw,se,sw,sw,") == 3);
+	REQUIRE(Count("ne,ne,ne,").first == 3);
+	REQUIRE(Count("ne,ne,sw,sw,").first == 0);
+	REQUIRE(Count("ne,ne,s,s,").first == 2);
+	REQUIRE(Count("se,sw,se,sw,sw,").first == 3);
 
 	std::ifstream ifs(INPUT);
 	std::string input;
 	std::getline(ifs, input);
 	input.push_back(',');
 
-	std::cout << Count(input.c_str()) << std::endl;
+	auto res = Count(input.c_str());
+	std::cout << res.first << std::endl;
+	std::cout << res.second << std::endl;
 }
