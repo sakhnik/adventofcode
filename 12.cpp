e26a3ccbc19d56c9217d6f3c7073ada3393c0367
Anatolii Sakhnik <sakhnik@gmail.com>
2017-12-12 08:11:14 +0200

    Use boost::graph

--- 12.cpp
+++ 12.cpp
@@ -1,90 +1,79 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 #include <iostream>
-#include <queue>
 #include <fstream>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/breadth_first_search.hpp>
 
-typedef std::vector<unsigned> AdjT;
-typedef std::vector<AdjT> GraphT;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> GraphT;
 
 GraphT ReadGraph(std::istream &is)
 {
-	GraphT graph;
+    GraphT graph;
 
-	std::string line;
-	while (std::getline(is, line))
-	{
-		std::istringstream iss(line);
+    std::string line;
+    while (std::getline(is, line))
+    {
+        std::istringstream iss(line);
 
-		unsigned v{0};
-		iss >> v;
-		if (v > graph.size())
-		{
-			graph.resize(v);
-		}
-		graph.emplace_back();
-		auto &adj = graph[v];
+        unsigned v{0};
+        iss >> v;
 
-		std::string skip;
-		iss >> skip;  // <->
+        std::string skip;
+        iss >> skip;  // <->
 
-		unsigned w;
-		while (iss >> w)
-		{
-			adj.push_back(w);
-			char ch{0};
-			iss >> ch;  // ,
-		}
-	}
+        unsigned w;
+        while (iss >> w)
+        {
+            boost::add_edge(v, w, graph);
+            char ch{0};
+            iss >> ch;  // ,
+        }
+    }
 
-	return graph;
+    return graph;
 }
 
 GraphT ReadGraph(std::istream &&is)
 {
-	return ReadGraph(is);
+    return ReadGraph(is);
 }
+
+class BfsCountingVisitor
+    : public boost::default_bfs_visitor
+{
+public:
+    BfsCountingVisitor(unsigned &count) : _count(count) { }
+
+    template <typename Vertex, typename Graph>
+        void discover_vertex(Vertex, const Graph &) const
+    {
+        ++_count;
+    }
+
+    unsigned &_count;
+};
 
 unsigned Count(const GraphT &graph)
 {
-	unsigned count{0};
-	std::vector<bool> visited(graph.size());
-	std::queue<unsigned> q;
-	q.push(0);
-	visited[0] = true;
-
-	while (!q.empty())
-	{
-		unsigned v = q.front();
-		q.pop();
-		visited[v] = true;
-		++count;
-
-		for (unsigned w : graph[v])
-		{
-			if (!visited[w])
-			{
-				q.push(w);
-				visited[w] = true;
-			}
-		}
-	}
-
-	return count;
+    unsigned count{0};
+    BfsCountingVisitor vis(count);
+    boost::breadth_first_search(graph, 0, boost::visitor(vis));
+    return count;
 }
 
 TEST_CASE("main")
 {
-	auto test_graph = ReadGraph(std::istringstream(R"(0 <-> 2
+    auto test_graph = ReadGraph(std::istringstream(R"(0 <-> 2
 1 <-> 1
 2 <-> 0, 3, 4
 3 <-> 2, 4
 4 <-> 2, 3, 6
 5 <-> 6
 6 <-> 4, 5)"));
-	REQUIRE(Count(test_graph) == 6);
+    REQUIRE(Count(test_graph) == 6);
 
-	std::ifstream ifs(INPUT);
-	auto graph = ReadGraph(ifs);
-	std::cout << Count(graph) << std::endl;
+    std::ifstream ifs(INPUT);
+    auto graph = ReadGraph(ifs);
+    std::cout << Count(graph) << std::endl;
 }
