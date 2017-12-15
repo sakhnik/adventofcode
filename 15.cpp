ee8ec160cac9867789d149c3a5780d05b6fcaf65
Anatolii Sakhnik <sakhnik@gmail.com>
2017-12-15 08:05:20 +0200

    Day 15-2

--- 15.cpp
+++ 15.cpp
@@ -17,9 +17,36 @@ unsigned Count(uint64_t a, uint64_t b)
 	return judge;
 }
 
+unsigned Count2(uint64_t a, uint64_t b)
+{
+	unsigned judge{0};
+
+	for (unsigned i = 0; i < 5000000; ++i)
+	{
+		do
+		{
+			a = (a * 16807) % 2147483647;
+		}
+		while (a & 3);
+
+		do
+		{
+			b = (b * 48271) % 2147483647;
+		}
+		while (b & 7);
+
+		if ((a & 0xffff) == (b & 0xffff))
+			++judge;
+	}
+
+	return judge;
+}
+
 TEST_CASE("main")
 {
 	REQUIRE(Count(65, 8921) == 588);
+	REQUIRE(Count2(65, 8921) == 309);
 
 	std::cout << Count(289, 629) << std::endl;
+	std::cout << Count2(289, 629) << std::endl;
 }
