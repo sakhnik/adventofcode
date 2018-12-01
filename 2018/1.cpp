e521db7ac00b06896639ed5fa5a5570f725c2dcf
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-01 07:25:11 +0200

    Solve 1-1

--- 2018/1.cpp
+++ 2018/1.cpp
@@ -1,6 +1,32 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 #include <iostream>
+#include <fstream>
 
-int main()
+int Sum(std::istream &is)
 {
-    return 0;
+    int res{};
+
+    int i{};
+    while (is && (is >> i))
+    {
+        res += i;
+    }
+
+    return res;
+}
+
+int Sum(std::istream &&is)
+{
+    return Sum(is);
+}
+
+TEST_CASE("main")
+{
+    REQUIRE(Sum(std::istringstream{"+1 -2 +3 +1"}) == 3);
+    REQUIRE(Sum(std::istringstream{"+1 +1 +1"}) == 3);
+    REQUIRE(Sum(std::istringstream{"+1 +1 -2"}) == 0);
+    REQUIRE(Sum(std::istringstream{"-1 -2 -3"}) == -6);
+
+    std::cout << Sum(std::ifstream(INPUT)) << std::endl;
 }
