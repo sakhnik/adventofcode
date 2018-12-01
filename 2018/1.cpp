732ffa866b9eb250d2e14946c102aa881fe78e68
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-01 08:38:47 +0200

    Day 1-2

--- 2018/1.cpp
+++ 2018/1.cpp
@@ -2,31 +2,63 @@
 #include <doctest/doctest.h>
 #include <iostream>
 #include <fstream>
+#include <unordered_set>
+#include <iterator>
+#include <numeric>
 
-int Sum(std::istream &is)
+using TuneT = std::vector<int>;
+
+TuneT Parse(std::istream &&is)
 {
-    int res{};
-
-    int i{};
-    while (is && (is >> i))
-    {
-        res += i;
-    }
-
-    return res;
+    return {std::istream_iterator<int>{is}, std::istream_iterator<int>{}};
 }
 
-int Sum(std::istream &&is)
+TuneT GetInput()
 {
-    return Sum(is);
+    return Parse(std::ifstream{INPUT});
+}
+
+TuneT GetSample(const char *s)
+{
+    return Parse(std::istringstream{s});
+}
+
+int Sum(const TuneT &tune)
+{
+    return std::accumulate(begin(tune), end(tune), 0);
+}
+
+int Twice(const TuneT &tune)
+{
+    std::unordered_set<int> sums{0};
+    int acc{};
+    while (true)
+    {
+        for (auto i : tune)
+        {
+            acc += i;
+            if (!sums.insert(acc).second)
+            {
+                return acc;
+            }
+        }
+    }
 }
 
 TEST_CASE("main")
 {
-    REQUIRE(Sum(std::istringstream{"+1 -2 +3 +1"}) == 3);
-    REQUIRE(Sum(std::istringstream{"+1 +1 +1"}) == 3);
-    REQUIRE(Sum(std::istringstream{"+1 +1 -2"}) == 0);
-    REQUIRE(Sum(std::istringstream{"-1 -2 -3"}) == -6);
+    REQUIRE(Sum(GetSample("+1 -2 +3 +1")) == 3);
+    REQUIRE(Sum(GetSample("+1 +1 +1")) == 3);
+    REQUIRE(Sum(GetSample("+1 +1 -2")) == 0);
+    REQUIRE(Sum(GetSample("-1 -2 -3")) == -6);
 
-    std::cout << Sum(std::ifstream(INPUT)) << std::endl;
+    auto input = GetInput();
+    std::cout << Sum(input) << std::endl;
+
+    REQUIRE(Twice(GetSample("+1 -1")) == 0);
+    REQUIRE(Twice(GetSample("+3 +3 +4 -2 -4")) == 10);
+    REQUIRE(Twice(GetSample("-6 +3 +8 +5 -6")) == 5);
+    REQUIRE(Twice(GetSample("+7 +7 -2 -7 -4")) == 14);
+
+    std::cout << Twice(input) << std::endl;
 }
