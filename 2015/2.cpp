fd173e4976216c1d522e2903db4241e64328fd03
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-02 08:33:58 +0200

    2015 day 2.2

--- 2015/2.cpp
+++ 2015/2.cpp
@@ -28,10 +28,17 @@ int CalcArea(const BoxT &box)
     return 3*box[0]*box[1] + 2*(box[1]*box[2] + box[0]*box[2]);
 }
 
-int CalcArea(const BoxesT &boxes)
+int CalcRibbon(const BoxT &box)
+{
+    REQUIRE(std::is_sorted(begin(box), end(box)));
+    return 2*(box[0] + box[1]) + box[0]*box[1]*box[2];
+}
+
+template <typename Func>
+int Accumulate(const BoxesT &boxes, Func func)
 {
     return std::accumulate(begin(boxes), end(boxes), 0,
-                           [](int a, const auto &b) { return a + CalcArea(b); });
+                           [func](int a, const auto &b) { return a + func(b); });
 }
 
 TEST_CASE("main")
@@ -39,5 +46,10 @@ TEST_CASE("main")
     REQUIRE(58 == CalcArea({2,3,4}));
     REQUIRE(43 == CalcArea({1,1,10}));
 
-    std::cout << CalcArea(GetInput()) << std::endl;
+    std::cout << Accumulate(GetInput(), CalcArea) << std::endl;
+
+    REQUIRE(34 == CalcRibbon({2,3,4}));
+    REQUIRE(14 == CalcRibbon({1,1,10}));
+
+    std::cout << Accumulate(GetInput(), CalcRibbon) << std::endl;
 }
