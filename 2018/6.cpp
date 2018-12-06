cc661cb139f86d7c047b9bace59ef8dc97c12c83
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-06 10:22:28 +0200

    2018 day 6.2

--- 2018/6.cpp
+++ 2018/6.cpp
@@ -2,6 +2,7 @@
 #include <doctest/doctest.h>
 #include <sstream>
 #include <fstream>
+#include <numeric>
 
 struct Point
 {
@@ -50,26 +51,45 @@ Box GetBox(const PointsT &points)
     return box;
 }
 
+class Field
+{
+public:
+    Field(const PointsT &points)
+        : _box{GetBox(points)}
+        , _width{_box.max.x - _box.min.x + 1}
+        , _height{_box.max.y - _box.min.y + 1}
+        , _field(_width*_height, -1)
+    {
+    }
+
+    int GetHeight() const { return _height; }
+    int GetWidth() const { return _width; }
+
+    std::vector<int>& Get() { return _field; }
+
+    int& GetXY(int x, int y)
+    {
+        return _field[y * _width + x];
+    }
+
+    // Count the Manhattan distance from a given position in the field
+    // to a point.
+    int Distance(int x, int y, const Point &p)
+    {
+        return std::abs(p.x - (x + _box.min.x)) + std::abs(p.y - (y + _box.min.y));
+    }
+
+private:
+    Box _box;
+    int _width, _height;
+    std::vector<int> _field;
+};
+
 // We'll be iterating over every position in the bounding box,
 // and determine the prevailing point for every position.
 int FindSpot(const PointsT &points)
 {
-    // Determine the bounding box.
-    auto box = GetBox(points);
-    auto width = box.max.x - box.min.x + 1;
-    auto height = box.max.y - box.min.y + 1;
-    std::vector<int> field(width*height, -1);
-
-    // Easy access the field.
-    auto mark = [&](int x, int y) -> int& {
-        return field[y * width + x];
-    };
-
-    // Count the Manhattan distance from a given position in the field
-    // to a point.
-    auto distance = [&](int x, int y, const Point &p) {
-        return std::abs(p.x - (x + box.min.x)) + std::abs(p.y - (y + box.min.y));
-    };
+    Field field(points);
 
     // Remember distances together with points.
     struct Dist
@@ -81,14 +101,14 @@ int FindSpot(const PointsT &points)
     std::vector<Dist> distances(points.size());
 
     // For every position in the field.
-    for (int y = 0; y < height; ++y)
+    for (int y = 0; y < field.GetHeight(); ++y)
     {
-        for (int x = 0; x < width; ++x)
+        for (int x = 0; x < field.GetWidth(); ++x)
         {
             // Calculate distances to every point.
             for (size_t i = 0; i < points.size(); ++i)
             {
-                distances[i].distance = distance(x, y, points[i]);
+                distances[i].distance = field.Distance(x, y, points[i]);
                 distances[i].pointNum = i;
             }
 
@@ -98,19 +118,20 @@ int FindSpot(const PointsT &points)
             // If they aren't tied (equal), mark the prevailing point on this position.
             if (distances[0].distance < distances[1].distance)
             {
-                mark(x, y) = distances[0].pointNum;
+                field.GetXY(x, y) = distances[0].pointNum;
             }
         }
     }
 
     // Ignore the ties.
-    auto it_end = std::remove(begin(field), end(field), -1);
+    auto &f = field.Get();
+    auto it_end = std::remove(begin(f), end(f), -1);
     // Sort the field to be able to calculate the most frequent point (biggest area).
-    std::sort(begin(field), it_end);
+    std::sort(begin(f), it_end);
 
     int max_area{0};
     int point = -1;
-    for (auto it = begin(field); it != it_end; )
+    for (auto it = begin(f); it != it_end; )
     {
         auto it2 = std::find_if_not(it, it_end,
                                     [pnum=*it](auto i) { return i == pnum; });
@@ -126,6 +147,32 @@ int FindSpot(const PointsT &points)
     return max_area;
 }
 
+int FindAreaWithin(const PointsT &points, int distance)
+{
+    Field field(points);
+
+    int count{0};
+
+    // For every position in the field.
+    for (int y = 0; y < field.GetHeight(); ++y)
+    {
+        for (int x = 0; x < field.GetWidth(); ++x)
+        {
+            // Calculate the sum of distances to every point.
+            auto d = std::accumulate(begin(points), end(points), 0,
+                            [&](int d, const auto &p) {
+                                return d + field.Distance(x, y, p);
+                            });
+            if (d < distance)
+            {
+                ++count;
+            }
+        }
+    }
+
+    return count;
+}
+
 TEST_CASE("main")
 {
     auto test = GetInput(std::istringstream("1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9"));
@@ -135,4 +182,7 @@ TEST_CASE("main")
 
     auto input = GetInput(std::ifstream(INPUT));
     std::cout << FindSpot(input) << std::endl;
+
+    REQUIRE(FindAreaWithin(test, 32) == 16);
+    std::cout << FindAreaWithin(input, 10000) << std::endl;
 }
