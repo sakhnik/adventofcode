93657ead61b0a481d6c48e81a40dca0f595ce1c8
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-07 21:48:32 +0200

    2018 day 7.2

--- 2018/7.cpp
+++ 2018/7.cpp
@@ -27,60 +27,162 @@ DepsT GetInput(std::istream &&is)
     return deps;
 }
 
-std::string SortSteps(DepsT &deps)
+struct Step
 {
-    struct Step
-    {
-        char name;
-        int deps = 0;
-        std::vector<char> notify;
-    };
+    char name;
+    int deps_count = 0;
+    std::vector<char> dependents;
+};
 
-    std::unordered_map<char, Step> steps_map;
+using StepsMapT = std::unordered_map<char, Step>;
+
+StepsMapT GetStepsMap(const DepsT &deps)
+{
+    StepsMapT steps_map;
     for (auto d : deps)
     {
         auto &s = steps_map[d.step];
         s.name = d.step;
-        s.notify.push_back(d.before);
+        s.dependents.push_back(d.before);
         steps_map[d.before].name = d.before;
-        ++steps_map[d.before].deps;
+        ++steps_map[d.before].deps_count;
     }
+    return steps_map;
+}
 
-    // Copy the data into a vector for sorting.
-    std::vector<Step*> steps;
-    for (auto &s : steps_map)
+class StepGenerator
+{
+public:
+    StepGenerator(const DepsT &deps)
+        : _steps_map{GetStepsMap(deps)}
     {
-        steps.push_back(&s.second);
-    }
-
-    auto order = [](const Step *a, const Step *b) {
-        // First come the steps with fewer dependent.
-        // NOTE the reversed order, because the heap is max-heap.
-        if (a->deps > b->deps)
-            return true;
-        if (a->deps < b->deps)
-            return false;
-        // Otherwise order them by name.
-        return a->name > b->name;
-    };
-
-    std::make_heap(begin(steps), end(steps), order);
-    std::string ret;
-
-    for (auto it = end(steps); it != begin(steps); )
-    {
-        std::pop_heap(begin(steps), it, order);
-        --it;
-        auto &step = *it;
-        ret.push_back(step->name);
-        for (char b : step->notify)
+        for (const auto &kv : _steps_map)
         {
-            --steps_map[b].deps;
+            _todo.push_back(kv.first);
         }
-        std::make_heap(begin(steps), it, order);
+        _done = 0;
+        _reported = 0;
+        _Sort();
     }
 
-    return ret;
+    char GetNextStep()
+    {
+        if (_reported >= _todo.size())
+        {
+            return -1;
+        }
+
+        auto s = _todo[_reported];
+        auto &step = _steps_map[s];
+        if (!step.deps_count)
+        {
+            // The step can be executed now, because all its dependencies are now
+            // satisfied.
+            ++_reported;
+            return s;
+        }
+
+        return -1;
+    }
+
+    void StepDone(char s)
+    {
+        auto it = std::find(begin(_todo) + _done, end(_todo), s);
+        REQUIRE(it - begin(_todo) <= _reported);
+
+        // The task is done, put it to the left.
+        std::swap(_todo[_done], *it);
+        ++_done;
+        REQUIRE(_done <= _reported);
+
+        // Update the dependent steps and resort.
+        for (auto d : _steps_map[s].dependents)
+        {
+            --_steps_map[d].deps_count;
+        }
+        _Sort();
+    }
+
+    const std::string& GetSorted()
+    {
+        return _todo;
+    }
+
+private:
+    StepsMapT _steps_map;  // Mutable information about the steps to do
+    std::string _todo;     // Ordered steps
+    size_t _done;          // The steps to the left are all done
+    size_t _reported;      // The steps to the left are all reported
+
+    void _Sort()
+    {
+        // Resort the steps not yet reported using their actual dependencies.
+        std::sort(begin(_todo) + _reported, end(_todo),
+                  [&](char a, char b) {
+                    const auto &sa = _steps_map[a];
+                    const auto &sb = _steps_map[b];
+                    if (sa.deps_count < sb.deps_count)
+                        return true;
+                    if (sa.deps_count > sb.deps_count)
+                        return false;
+                    return a < b;
+                  });
+    }
+};
+
+std::string SortSteps(const DepsT &deps)
+{
+    StepGenerator gen(deps);
+    while (true)
+    {
+        auto s = gen.GetNextStep();
+        if (s == -1)
+        {
+            break;
+        }
+        gen.StepDone(s);
+    }
+    return gen.GetSorted();
+}
+
+int Simulate(int concurrency, int time_add, const DepsT &deps)
+{
+    StepGenerator gen(deps);
+    struct Worker
+    {
+        char step;
+        int time_left = 0;
+    };
+    std::vector<Worker> workers;
+
+    int time{0};
+    while (true)
+    {
+        auto it = std::remove_if(begin(workers), end(workers),
+                                 [&](auto &w) {
+                                    if (--w.time_left > 0)
+                                        return false;
+                                    gen.StepDone(w.step);
+                                    return true;
+                                 });
+        workers.erase(it, end(workers));
+
+        for (int i = size(workers); i < concurrency; ++i)
+        {
+            char s = gen.GetNextStep();
+            if (-1 == s)
+            {
+                break;
+            }
+            workers.push_back({s, time_add + 1 + s - 'A'});
+        }
+
+        if (workers.empty())
+            break;
+
+        ++time;
+    }
+    return time;
 }
 
 TEST_CASE("main")
@@ -97,4 +199,7 @@ Step F must be finished before step E can begin.)";
 
     auto input = GetInput(std::ifstream(INPUT));
     std::cout << SortSteps(input) << std::endl;
+
+    REQUIRE(15 == Simulate(2, 0, test));
+    std::cout << Simulate(5, 60, input) << std::endl;
 }
