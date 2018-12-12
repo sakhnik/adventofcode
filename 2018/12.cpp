a6845595905d850c0247a1adf6e2af347c63ad9f
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-12 11:04:21 +0200

    2018 day 12.2

--- 2018/12.cpp
+++ 2018/12.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <fstream>
 #include <algorithm>
+#include <iostream>
 
 namespace {
 
@@ -20,26 +21,93 @@ public:
         }
     }
 
-    void Evolve()
+    void Evolve(int64_t steps)
     {
-        std::string new_pots;
-        int idx = _initial_num - 3;
+        for (int64_t i = 0; i < steps; ++i)
+        {
+            _DoEvolve();
+
+            if (_pots == _prev_pots)
+            {
+                // Reached a stable pattern. No need to cycle anymore.
+                int64_t dn = _initial_num - _prev_initial_num;
+                _initial_num += dn * (steps - i - 1);
+                _prev_initial_num = _initial_num - dn;
+                return;
+            }
+        }
+    }
+
+    const std::string& GetPots() const
+    {
+        return _pots;
+    }
+
+    int64_t GetInitialNum() const
+    {
+        return _initial_num;
+    }
+
+    int64_t CalcPositionsSum() const
+    {
+        auto idx = _initial_num;
+        int64_t sum = 0;
+
+        for (auto ch : _pots)
+        {
+            if (ch == '#')
+                sum += idx;
+            ++idx;
+        }
+
+        return sum;
+    }
+
+private:
+    int64_t _initial_num = 0;
+    int64_t _prev_initial_num = 0;
+    std::string _pots;
+    std::string _prev_pots;
+    bool _rules[1 << 5] = { 0 };
+    bool _stable_detected = false;
+
+    uint8_t _Parse(const char *s)
+    {
+        uint8_t ret{0};
+        for (int i = 0; i < 5; ++i, ++s)
+        {
+            ret <<= 1;
+            ret |= _GetVal(*s);
+        }
+        return ret;
+    }
+
+    uint8_t _GetVal(char pot)
+    {
+        return pot == '#' ? 1 : 0;
+    }
+
+    void _DoEvolve()
+    {
+        _prev_pots.clear();
+        int64_t idx = _initial_num - 3;
 
         auto applyRules = [&](auto config) {
             auto res = _rules[config];
             if (res)
             {
-                if (new_pots.empty())
+                if (_prev_pots.empty())
                 {
+                    _prev_initial_num = _initial_num;
                     _initial_num = idx;
                 }
-                new_pots.push_back('#');
+                _prev_pots.push_back('#');
             }
             else
             {
-                if (!new_pots.empty())
+                if (!_prev_pots.empty())
                 {
-                    new_pots.push_back('.');
+                    _prev_pots.push_back('.');
                 }
             }
             ++idx;
@@ -65,58 +133,12 @@ public:
             applyRules(cur_config);
         }
 
-        while (new_pots.back() == '.')
+        while (_prev_pots.back() == '.')
         {
-            new_pots.pop_back();
+            _prev_pots.pop_back();
         }
 
-        _pots.swap(new_pots);
-    }
-
-    const std::string& GetPots() const
-    {
-        return _pots;
-    }
-
-    int GetInitialNum() const
-    {
-        return _initial_num;
-    }
-
-    int CalcPositionsSum() const
-    {
-        auto idx = _initial_num;
-        auto sum = 0;
-
-        for (auto ch : _pots)
-        {
-            if (ch == '#')
-                sum += idx;
-            ++idx;
-        }
-
-        return sum;
-    }
-
-private:
-    int _initial_num = 0;
-    std::string _pots;
-    bool _rules[1 << 5] = { 0 };
-
-    uint8_t _Parse(const char *s)
-    {
-        uint8_t ret{0};
-        for (int i = 0; i < 5; ++i, ++s)
-        {
-            ret <<= 1;
-            ret |= _GetVal(*s);
-        }
-        return ret;
-    }
-
-    uint8_t _GetVal(char pot)
-    {
-        return pot == '#' ? 1 : 0;
+        _pots.swap(_prev_pots);
     }
 };
 
@@ -143,27 +165,23 @@ TEST_CASE(TEST_NAME)
 ####. => #)";
         Plants plants(std::istringstream{TEST_INPUT});
         REQUIRE("#..#.#..##......###...###" == plants.GetPots());
-        plants.Evolve();
+        plants.Evolve(1);
         REQUIRE("#...#....#.....#..#..#..#" == plants.GetPots());
-        plants.Evolve();
+        plants.Evolve(1);
         REQUIRE("##..##...##....#..#..#..##" == plants.GetPots());
-        plants.Evolve();
+        plants.Evolve(1);
         REQUIRE("#.#...#..#.#....#..#..#...#" == plants.GetPots());
-        for (int i = 0; i < 17; ++i)
-        {
-            plants.Evolve();
-        }
+        plants.Evolve(17);
         REQUIRE("#....##....#####...#######....#.#..##" == plants.GetPots());
         REQUIRE(-2 == plants.GetInitialNum());
         REQUIRE(325 == plants.CalcPositionsSum());
     }
 
-    SUBCASE("task1") {
+    SUBCASE("tasks") {
         Plants plants(std::ifstream{INPUT});
-        for (int i = 0; i < 20; ++i)
-        {
-            plants.Evolve();
-        }
+        plants.Evolve(20);
+        MESSAGE(plants.CalcPositionsSum());
+        plants.Evolve(50000000000 - 20);
         MESSAGE(plants.CalcPositionsSum());
     }
 }
