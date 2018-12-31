8dadf13ab3d905696742eb928ea0092ef6a15a08
Anatolii Sakhnik <sakhnik@gmail.com>
2018-12-31 08:57:28 +0200

    2015 day 6.2

--- 2015/06.cpp
+++ 2015/06.cpp
@@ -6,74 +6,88 @@
 
 namespace {
 
-class Lights
-{
-public:
-    Lights(std::ifstream &&is)
-        : _lights(1000*1000, 0)
-    {
-        std::string line;
-        while (is && getline(is, line))
-        {
-            static const std::regex r(R"((turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+))");
-            std::smatch m;
-            REQUIRE(std::regex_match(line, m, r));
-            int r0 = std::stoi(m[2]);
-            int c0 = std::stoi(m[3]);
-            int r1 = std::stoi(m[4]);
-            int c1 = std::stoi(m[5]);
+using LightsT = std::vector<int>;
+const size_t WIDTH = 1000;
 
-            for (int r = r0; r <= r1; ++r)
+void On(int row, int col, LightsT &lights)
+{
+    lights[row * WIDTH + col] = 1;
+}
+
+void Off(int row, int col, LightsT &lights)
+{
+    lights[row * WIDTH + col] = 0;
+}
+
+void Toggle(int row, int col, LightsT &lights)
+{
+    auto &v = lights[row * WIDTH + col];
+    v = 1 - v;
+}
+
+void On2(int row, int col, LightsT &lights)
+{
+    ++lights[row * WIDTH + col];
+}
+
+void Off2(int row, int col, LightsT &lights)
+{
+    auto &v = lights[row * WIDTH + col];
+    if (v > 0)
+        --v;
+}
+
+void Toggle2(int row, int col, LightsT &lights)
+{
+    auto &v = lights[row * WIDTH + col];
+    v += 2;
+}
+
+template <typename OnT, typename OffT, typename ToggleT>
+size_t CountLit(std::ifstream &&is, OnT on, OffT off, ToggleT toggle)
+{
+    LightsT lights(WIDTH*WIDTH, 0);
+
+    std::string line;
+    while (is && getline(is, line))
+    {
+        static const std::regex r(R"((turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+))");
+        std::smatch m;
+        REQUIRE(std::regex_match(line, m, r));
+        int r0 = std::stoi(m[2]);
+        int c0 = std::stoi(m[3]);
+        int r1 = std::stoi(m[4]);
+        int c1 = std::stoi(m[5]);
+
+        for (int r = r0; r <= r1; ++r)
+        {
+            for (int c = c0; c <= c1; ++c)
             {
-                for (int c = c0; c <= c1; ++c)
+                if (m[1] == "turn on")
                 {
-                    if (m[1] == "turn on")
-                    {
-                        _On(r, c);
-                    }
-                    else if (m[1] == "turn off")
-                    {
-                        _Off(r, c);
-                    }
-                    else //if (m[1] == "toggle")
-                    {
-                        _Toggle(r, c);
-                    }
+                    on(r, c, lights);
+                }
+                else if (m[1] == "turn off")
+                {
+                    off(r, c, lights);
+                }
+                else //if (m[1] == "toggle")
+                {
+                    toggle(r, c, lights);
                 }
             }
         }
     }
 
-    size_t GetLitCount() const
-    {
-        return std::accumulate(_lights.begin(), _lights.end(), 0u);
-    }
-
-private:
-    std::vector<uint8_t> _lights;
-
-    void _On(int row, int col)
-    {
-        _lights[row * 1000 + col] = 1;
-    }
-
-    void _Off(int row, int col)
-    {
-        _lights[row * 1000 + col] = 0;
-    }
-
-    void _Toggle(int row, int col)
-    {
-        auto &v = _lights[row * 1000 + col];
-        v = 1 - v;
-    }
-};
+    return std::accumulate(lights.begin(), lights.end(), 0u);
+}
 
 } //namespace;
 
 TEST_CASE(TEST_NAME)
 {
     SUBCASE("task") {
-        MESSAGE(Lights(std::ifstream{INPUT}).GetLitCount());
+        MESSAGE(CountLit(std::ifstream{INPUT}, On, Off, Toggle));
+        MESSAGE(CountLit(std::ifstream{INPUT}, On2, Off2, Toggle2));
     }
 }
