34960f0cabac9e1dcce04611be205278bfbca4be
Anatolii Sakhnik <sakhnik@gmail.com>
2019-01-01 19:37:26 +0200

    2015 day 9.1

---
+++ 2015/09.cpp
@@ -0,0 +1,98 @@
+#include <doctest/doctest.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+
+namespace {
+
+class Tsp
+{
+public:
+    Tsp(std::istream &&is)
+    {
+        std::string city, prev, skip;
+        int dist;
+        int i{0}, j{1};
+        while (is && (is >> city >> skip >> skip >> skip >> dist))
+        {
+            if (prev.empty())
+            {
+                prev = city;
+            }
+            else if (prev != city)
+            {
+                prev = city;
+                if (!_ncities)
+                {
+                    _ncities = j;
+                }
+                j = i + 2;
+                ++i;
+            }
+
+            _dist[i][j] = dist;
+            _dist[j][i] = dist;
+            ++j;
+        }
+
+        for (int i = 0; i < _ncities; ++i)
+        {
+            _dist[i][i] = 0;
+            //for (int j = 0; j < _ncities; ++j)
+            //{
+            //    std::cout << "  " << _dist[i][j];
+            //}
+            //std::cout << "\n";
+        }
+    }
+
+    int FindShortest() const
+    {
+        int idx[_ncities];
+        std::iota(idx, idx + _ncities, 0);
+        auto dist = [this](int *idx) {
+            int d{};
+            for (int i = 1; i < _ncities; ++i)
+            {
+                d += _dist[idx[i-1]][idx[i]];
+            }
+            return d;
+        };
+        int min_dist = dist(idx);
+        while (std::next_permutation(idx, idx + _ncities))
+        {
+            auto d2 = dist(idx);
+            if (d2 < min_dist)
+            {
+                min_dist = d2;
+            }
+        }
+        return min_dist;
+    }
+
+private:
+    int _ncities{};
+    int _dist[10][10];
+};
+
+} //namespace;
+
+TEST_CASE(TEST_NAME)
+{
+    SUBCASE("test") {
+        const char *const TEST =
+            "London to Dublin = 464\n"
+            "London to Belfast = 518\n"
+            "Dublin to Belfast = 141\n";
+        Tsp tsp(std::istringstream{TEST});
+        REQUIRE(605 == tsp.FindShortest());
+
+    }
+
+    SUBCASE("task") {
+        Tsp tsp(std::ifstream{INPUT});
+        MESSAGE(tsp.FindShortest());
+    }
+}
