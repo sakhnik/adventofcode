f40faa186e14a1eca3c96e6cf71ebce31f2b840e
Anatolii Sakhnik <sakhnik@gmail.com>
2019-01-04 21:07:19 +0200

    2015 day 13.1

---
+++ 2015/13.cpp
@@ -0,0 +1,118 @@
+#include <doctest/doctest.h>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <algorithm>
+
+namespace {
+
+class Table
+{
+public:
+    Table(std::istream &&is)
+    {
+        std::string line;
+        while (is && getline(is, line))
+        {
+            char a[64], b[64], effect[64];
+            int amount{};
+
+            sscanf(line.c_str(), "%s would %s %d happiness units by sitting next to %s.", a, effect, &amount, b);
+            int i = _GetNameIdx(a);
+            int j = _GetNameIdx(b);
+            if (std::string_view{effect} == "lose")
+                amount *= -1;
+            _points[i][j] = amount;
+        }
+
+        for (size_t i = 0; i < _names.size(); ++i)
+            _points[i][i] = 0;
+    }
+
+    void Print() const
+    {
+        for (size_t i = 0; i < _names.size(); ++i)
+        {
+            for (size_t j = 0; j < _names.size(); ++j)
+            {
+                printf("%4d", _points[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    int Arrange() const
+    {
+        int idx[_names.size()];
+        std::iota(idx, idx + _names.size(), 0);
+
+        auto count = [this](int idx[]) {
+            int res{};
+            for (size_t i = 0; i < _names.size(); ++i)
+            {
+                size_t j = (i + _names.size() + 1) % _names.size();
+                res += _points[idx[i]][idx[j]];
+                res += _points[idx[j]][idx[i]];
+            }
+            return res;
+        };
+
+        int max_points = std::numeric_limits<int>::min();
+        while (std::next_permutation(idx, idx + _names.size()))
+        {
+            auto c = count(idx);
+            if (c > max_points)
+            {
+                max_points = c;
+            }
+        }
+
+        return max_points;
+    }
+
+private:
+    std::unordered_map<std::string, int> _names;
+    int _points[10][10];
+
+    int _GetNameIdx(std::string name)
+    {
+        while (name.back() == '.')
+            name.pop_back();
+
+        auto it = _names.find(name);
+        if (it != _names.end())
+        {
+            return it->second;
+        }
+        return _names[name] = _names.size();
+    }
+};
+
+} //namespace;
+
+TEST_CASE(TEST_NAME)
+{
+    SUBCASE("test") {
+        const char *const TEST =
+            "Alice would gain 54 happiness units by sitting next to Bob.\n"
+            "Alice would lose 79 happiness units by sitting next to Carol.\n"
+            "Alice would lose 2 happiness units by sitting next to David.\n"
+            "Bob would gain 83 happiness units by sitting next to Alice.\n"
+            "Bob would lose 7 happiness units by sitting next to Carol.\n"
+            "Bob would lose 63 happiness units by sitting next to David.\n"
+            "Carol would lose 62 happiness units by sitting next to Alice.\n"
+            "Carol would gain 60 happiness units by sitting next to Bob.\n"
+            "Carol would gain 55 happiness units by sitting next to David.\n"
+            "David would gain 46 happiness units by sitting next to Alice.\n"
+            "David would lose 7 happiness units by sitting next to Bob.\n"
+            "David would gain 41 happiness units by sitting next to Carol.\n";
+        Table t(std::istringstream{TEST});
+        REQUIRE(330 == t.Arrange());
+    }
+
+    SUBCASE("task") {
+        Table t(std::ifstream{INPUT});
+        MESSAGE(t.Arrange());
+    }
+}
