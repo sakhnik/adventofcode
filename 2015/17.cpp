3d28678143d0fbc3f1af2e9a39bbb2ee4cc31adb
Anatolii Sakhnik <sakhnik@gmail.com>
2019-01-07 18:31:34 +0200

    2015 day 17.2

--- 2015/17.cpp
+++ 2015/17.cpp
@@ -1,5 +1,6 @@
 #include <doctest/doctest.h>
 #include <vector>
+#include <limits>
 
 namespace {
 
@@ -14,6 +15,46 @@ int Count(int target, IterT first, IterT last)
     return Count(target - *first, first + 1, last) + Count(target, first + 1, last);
 }
 
+class Counter
+{
+public:
+    template <typename IterT>
+    void Iterate(int target, IterT first, IterT last, int pots)
+    {
+        if (target == 0)
+        {
+            if (min_pots < pots)
+                return;
+            if (min_pots == pots)
+                ++count;
+            else
+            {
+                min_pots = pots;
+                count = 1;
+            }
+            return;
+        }
+        if (target < 0 || first == last)
+            return;
+        Iterate(target - *first, first + 1, last, pots + 1);
+        Iterate(target, first + 1, last, pots);
+    }
+
+    int GetCount() const { return count; }
+
+private:
+    int min_pots = std::numeric_limits<int>::max();
+    int count = 0;
+};
+
+template <typename IterT>
+int Count2(int target, IterT first, IterT last)
+{
+    Counter counter;
+    counter.Iterate(target, first, last, 0);
+    return counter.GetCount();
+}
+
 } //namespace;
 
 TEST_CASE(TEST_NAME)
@@ -21,6 +62,7 @@ TEST_CASE(TEST_NAME)
     SUBCASE("test") {
         int sizes[] = {20, 15, 10, 5, 5};
         REQUIRE(4 == Count(25, std::begin(sizes), std::end(sizes)));
+        REQUIRE(3 == Count2(25, std::begin(sizes), std::end(sizes)));
     }
 
     SUBCASE("task") {
@@ -29,5 +71,6 @@ TEST_CASE(TEST_NAME)
             32, 36, 15, 11, 46, 26, 28, 1, 19, 3
         };
         MESSAGE(Count(150, std::begin(sizes), std::end(sizes)));
+        MESSAGE(Count2(150, std::begin(sizes), std::end(sizes)));
     }
 }
