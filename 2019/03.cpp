f99eb9cd7719c21831d20861ab965554bb66fa4c
Anatolii Sakhnik <sakhnik@gmail.com>
2019-12-03 15:36:52 +0200

    2019 day 3.2

--- 2019/03.cpp
+++ 2019/03.cpp
@@ -7,43 +7,61 @@
 #include <fstream>
 
 
-enum Direction
-{
-    HORIZONTAL = 0,
-    VERTICAL
-};
-
 struct Segment
 {
-    Direction dir;
+    char direction;
     int value;  // height, for instance
     int start;
-    int end;    // start <= end
+    int end;
+    int distance;
+
+    bool isHorizontal() const
+    {
+        return direction == 'L' || direction == 'R';
+    }
 };
 
 int GetIntersection(const Segment &a, const Segment &b)
 {
-    if (a.dir != b.dir)
+    if (a.isHorizontal() != b.isHorizontal())
     {
-        assert(a.start <= a.end);
-        assert(b.start <= b.end);
-        if ((a.value >= b.start && a.value <= b.end) &&
-            (b.value >= a.start && b.value <= a.end))
+        double r1 = 1. * (a.value - b.start) / (b.end - b.start);
+        double r2 = 1. * (b.value - a.start) / (a.end - a.start);
+        if ((r1 >= 0. && r1 <= 1.) && (r2 >= 0. && r2 <= 1.))
         {
             return std::abs(a.value) + std::abs(b.value);
         }
         return std::numeric_limits<int>::max();
     }
 
-    // The lines are parallel
-    if (a.value != b.value)
-    {
-        return std::numeric_limits<int>::max();
-    }
+    //// The lines are parallel
+    //if (a.value != b.value)
+    //{
+    //    return std::numeric_limits<int>::max();
+    //}
 
     // TODO: colinear
     //auto start = std::max(a.start, b.start);
     //auto end = std::min(a.end, b.end);
+    return std::numeric_limits<int>::max();
+}
+
+int GetIntersection2(const Segment &a, const Segment &b)
+{
+    if (a.isHorizontal() != b.isHorizontal())
+    {
+        double r1 = 1. * (a.value - b.start) / (b.end - b.start);
+        double r2 = 1. * (b.value - a.start) / (a.end - a.start);
+        if ((r1 >= 0. && r1 <= 1.) && (r2 >= 0. && r2 <= 1.))
+        {
+            return a.distance + b.distance
+                + static_cast<int>(0.5
+                                   + r1 * std::abs(b.end - b.start)
+                                   + r2 * std::abs(a.end - a.start));
+        }
+        return std::numeric_limits<int>::max();
+    }
+
     return std::numeric_limits<int>::max();
 }
 
@@ -56,6 +74,7 @@ PathT ParseInput(const std::string &input)
     int x{}, y{};
     char direction{}, comma{};
     int length{};
+    int distance{};
     std::istringstream iss{input};
 
     while (iss)
@@ -64,35 +83,37 @@ PathT ParseInput(const std::string &input)
         switch (direction)
         {
         case 'R':
-            path.push_back({HORIZONTAL, y, x, x + length});
+            path.push_back({direction, y, x, x + length, distance});
             x += length;
             break;
         case 'D':
-            path.push_back({VERTICAL, x, y - length, y});
+            path.push_back({direction, x, y, y - length, distance});
             y -= length;
             break;
         case 'L':
-            path.push_back({HORIZONTAL, y, x - length, x});
+            path.push_back({direction, y, x, x - length, distance});
             x -= length;
             break;
         case 'U':
-            path.push_back({VERTICAL, x, y, y + length});
+            path.push_back({direction, x, y, y + length, distance});
             y += length;
             break;
         }
+        distance += length;
     }
 
     return path;
 }
 
-int FindClosestIntersection(const PathT &p1, const PathT &p2)
+template <typename FuncT>
+int FindClosestIntersection(const PathT &p1, const PathT &p2, FuncT func)
 {
     int dist{std::numeric_limits<int>::max()};
     for (const auto &a : p1)
     {
         for (const auto &b : p2)
         {
-            auto d = GetIntersection(a, b);
+            auto d = func(a, b);
             if (d && d < dist)
             {
                 dist = d;
@@ -104,11 +125,15 @@ int FindClosestIntersection(const PathT &p1, const PathT &p2)
 
 TEST_CASE(TEST_NAME)
 {
-    REQUIRE(6 == FindClosestIntersection(ParseInput("R8,U5,L5,D3"), ParseInput("U7,R6,D4,L4")));
+    REQUIRE(6 == FindClosestIntersection(ParseInput("R8,U5,L5,D3"),
+                                         ParseInput("U7,R6,D4,L4"),
+                                         GetIntersection));
     REQUIRE(159 == FindClosestIntersection(ParseInput("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
-                                           ParseInput("U62,R66,U55,R34,D71,R55,D58,R83")));
+                                           ParseInput("U62,R66,U55,R34,D71,R55,D58,R83"),
+                                           GetIntersection));
     REQUIRE(135 == FindClosestIntersection(ParseInput("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
-                                           ParseInput("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")));
+                                           ParseInput("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
+                                           GetIntersection));
 
     std::ifstream ifs(INPUT);
     std::string s;
@@ -116,5 +141,17 @@ TEST_CASE(TEST_NAME)
     auto p1 = ParseInput(s);
     std::getline(ifs, s);
     auto p2 = ParseInput(s);
-    MESSAGE(FindClosestIntersection(p1, p2));
+    MESSAGE(FindClosestIntersection(p1, p2, GetIntersection));
+
+
+    REQUIRE(30 == FindClosestIntersection(ParseInput("R8,U5,L5,D3"),
+                                          ParseInput("U7,R6,D4,L4"),
+                                          GetIntersection2));
+    REQUIRE(610 == FindClosestIntersection(ParseInput("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+                                           ParseInput("U62,R66,U55,R34,D71,R55,D58,R83"),
+                                           GetIntersection2));
+    REQUIRE(410 == FindClosestIntersection(ParseInput("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+                                           ParseInput("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
+                                           GetIntersection2));
+    MESSAGE(FindClosestIntersection(p1, p2, GetIntersection2));
 }
