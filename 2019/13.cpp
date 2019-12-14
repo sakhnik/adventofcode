7efd2cca5e0ac326b773a6b15415ede898ccec87
Anatolii Sakhnik <sakhnik@gmail.com>
2019-12-14 20:47:14 +0200

    2019 day 13.2

--- 2019/13.cpp
+++ 2019/13.cpp
@@ -8,22 +8,84 @@ TEST_CASE(TEST_NAME)
     std::ifstream ifs{INPUT};
     IntCode prog{ifs};
 
-    int count{0};
-
-    while (prog.GetState() != prog.S_HALT)
     {
-        /*auto x =*/ prog.Advance(0);
-        if (prog.GetState() == prog.S_HALT)
-            break;
-        REQUIRE(prog.GetState() == prog.S_OUTPUT);
-        /*auto y =*/ prog.Advance(0);
-        REQUIRE(prog.GetState() == prog.S_OUTPUT);
-        auto kind = prog.Advance(0);
-        if (prog.GetState() == prog.S_OUTPUT && kind == 2)
+        IntCode p1{prog};
+        int count{0};
+
+        while (p1.GetState() != p1.S_HALT)
         {
-            ++count;
+            /*auto x =*/ p1.Advance(0);
+            if (p1.GetState() == p1.S_HALT)
+                break;
+            REQUIRE(p1.GetState() == p1.S_OUTPUT);
+            /*auto y =*/ p1.Advance(0);
+            REQUIRE(p1.GetState() == p1.S_OUTPUT);
+            auto kind = p1.Advance(0);
+            if (p1.GetState() == p1.S_OUTPUT && kind == 2)
+            {
+                ++count;
+            }
         }
+
+        MESSAGE(count);
     }
 
-    MESSAGE(count);
+    {
+        IntCode p2{prog};
+        p2.SetMemory(0, 2);
+        int joystick{0};
+
+        int r{};
+        int score{};
+        int pad_x{}, pad_y{};
+        int ball_x{}, ball_y{};
+
+        while (p2.GetState() != p2.S_HALT)
+        {
+            switch (p2.GetState())
+            {
+            case p2.S_RUN:
+                r = p2.Advance(0);
+                break;
+            case p2.S_OUTPUT:
+                {
+                    int x = r;
+                    int y = p2.Advance(0);
+                    CHECK(p2.GetState() == p2.S_OUTPUT);
+                    int z = p2.Advance(0);
+                    CHECK(p2.GetState() == p2.S_OUTPUT);
+                    if (x == -1)
+                    {
+                        score = z;
+                    }
+                    else if (z == 3)
+                    {
+                        pad_x = x;
+                        pad_y = y;
+                    }
+                    else if (z == 4)
+                    {
+                        ball_x = x;
+                        ball_y = y;
+                    }
+                    r = p2.Advance(0);
+                }
+                break;
+            case p2.S_INPUT:
+                r = p2.Advance(joystick);
+                break;
+            default:
+                break;
+            }
+
+            if (ball_x < pad_x)
+                joystick = -1;
+            else if (ball_x > pad_x)
+                joystick = 1;
+            else
+                joystick = 0;
+        }
+
+        MESSAGE(score);
+    }
 }
