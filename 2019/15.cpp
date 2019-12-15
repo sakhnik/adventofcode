a624ababab68ed5ea43e99500b5064f5d67222fc
Anatolii Sakhnik <sakhnik@gmail.com>
2019-12-15 22:29:53 +0200

    2019 day 15.2

--- 2019/15.cpp
+++ 2019/15.cpp
@@ -7,6 +7,7 @@
 
 namespace {
 
+// Ordered position on the map
 struct Pos
 {
     int x,y;
@@ -29,11 +30,10 @@ struct Pos
 
 TEST_CASE(TEST_NAME)
 {
-    srand((unsigned)time(0));
-
     std::ifstream ifs{INPUT};
     IntCode prog{ifs};
 
+    // Let's explore the map first
     Pos robot{}, target{};
     std::map<Pos, char> map{{robot, '.'}};
 
@@ -70,6 +70,7 @@ TEST_CASE(TEST_NAME)
         }
     };
 
+    // Calculate the next position of the robot in the given direction.
     auto nextPos = [&](int direction) {
         Pos ret{robot};
         switch (direction)
@@ -82,15 +83,18 @@ TEST_CASE(TEST_NAME)
         return ret;
     };
 
+    // The input to the firmware
     int input{1};
+    // Exploration track node
     struct Track
     {
-        Pos pos;
-        int direction = 0;
-        int from = 0;
+        Pos pos;              // position
+        int direction = 0;    // yet non-explored directions (1..4)
+        int from = 0;         // from which direction we came here
     };
-    std::vector<Track> track{{robot}};
+    std::vector<Track> track{{robot}};   // backtracking path
 
+    // Step into the unknown (given direction)
     auto createNextTrack = [&](int direction) {
         Track t;
         t.pos = nextPos(direction);
@@ -99,11 +103,13 @@ TEST_CASE(TEST_NAME)
         return t;
     };
 
+    // Exploration process
     auto explore = [&]() {
         while (!track.empty())
         {
             auto &t = track.back();
 
+            // If probed all possible directions, backtrack.
             if (t.direction >= 4)
             {
                 // backtrack
@@ -112,6 +118,7 @@ TEST_CASE(TEST_NAME)
                 return;
             }
 
+            // Try the next direction
             input = ++t.direction;
             auto robotNext = createNextTrack(input);
             if (!map.count(robotNext.pos))
@@ -120,9 +127,11 @@ TEST_CASE(TEST_NAME)
                 return;
             }
         }
+        // If nowhere to backtrack, end the program.
         input = 0;
     };
 
+    // Start exploring
     explore();
 
     while (prog.GetState() != prog.S_HALT)
@@ -138,18 +147,21 @@ TEST_CASE(TEST_NAME)
         case prog.S_OUTPUT:
             if (r == 0)
             {
+                // No way, the wall
                 const auto &t = track.back();
                 map.emplace(t.pos, '#');
                 track.pop_back();  // dead end
             }
             else if (r == 1)
             {
+                // Passage
                 const auto &t = track.back();
                 map.emplace(t.pos, '.');
                 robot = t.pos;
             }
             else if (r == 2)
             {
+                // Oxygen target found
                 const auto &t = track.back();
                 map.emplace(t.pos, '.');
                 target = robot = t.pos;
@@ -160,10 +172,11 @@ TEST_CASE(TEST_NAME)
 
     printMap();
 
-    auto bfs = [&]() {
-        std::map<Pos, int> distance{{{0,0}, 0}};
+    // Run BFS from the given position, return the map of distances.
+    auto bfs = [&](Pos start) {
+        std::map<Pos, int> distance{{start, 0}};
         std::queue<Pos> todo;
-        todo.push({0,0});
+        todo.push(start);
 
         while (!todo.empty())
         {
@@ -187,8 +200,20 @@ TEST_CASE(TEST_NAME)
             probe({pos.x + 1, pos.y});
         }
 
-        return distance.at(target);
+        return distance;
     };
 
-    MESSAGE(bfs());
+    {
+        // Task 1: the distance to the oxygen.
+        auto distance = bfs({0,0});
+        MESSAGE(distance.at(target));
+    }
+
+    {
+        // Task 2: the farthest distance from the oxygen.
+        auto distance = bfs(target);
+        auto it = std::max_element(begin(distance), end(distance),
+                                   [](const auto &a, const auto &b) { return a.second < b.second; });
+        MESSAGE(it->second);
+    }
 }
