c3a56ad941a0fc43ff138c8e2246c27286b8f66c
Anatolii Sakhnik <sakhnik@gmail.com>
2019-12-21 14:54:37 +0200

    2019 day 21.2

--- 2019/21.cpp
+++ 2019/21.cpp
@@ -3,6 +3,30 @@
 #include <fstream>
 
 
+int Survey(const char *script, IntCode prog)
+{
+    int input = 0;
+    while (prog.GetState() != prog.S_HALT)
+    {
+        auto r = prog.Advance(input);
+        switch (prog.GetState())
+        {
+        case IntCode::S_RUN:
+        case IntCode::S_HALT:
+            continue;
+        case IntCode::S_OUTPUT:
+            if (r >= 128)
+                return r;
+            std::cout << static_cast<char>(r) << std::flush;
+            break;
+        case IntCode::S_INPUT:
+            input = *script++;
+            break;
+        }
+    }
+    return -1;
+}
+
 TEST_CASE(TEST_NAME)
 {
     std::ifstream ifs{INPUT};
@@ -11,33 +35,30 @@ TEST_CASE(TEST_NAME)
     // The bot always jumps 4 squares ahead.
     // So it should jump as soon as a hole is detected,
     // but having the ground on the fourth tile.
-    const char *script =
-        "NOT A J\n"
-        "NOT B T\n"
-        "OR J T\n"
-        "NOT C J\n"
-        "OR T J\n"
-        "AND D J\n"
+    const char *walk =
+        "NOT T T\n"  // T = true
+        "AND A T\n"  // T = A
+        "AND B T\n"  // T = A && B
+        "AND C T\n"  // T = A && B && C
+        "NOT T T\n"  // T = !(A && B && C)  <- hole ahead
+        "NOT J J\n"  // J = true
+        "AND T J\n"  // J = T
+        "AND D J\n"  // J = T && D
         "WALK\n";
+    MESSAGE(Survey(walk, prog));
 
-    int input = 0;
-    while (prog.GetState() != prog.S_HALT)
-    {
-        auto r = prog.Advance(input);
-        switch (prog.GetState())
-        {
-        case prog.S_RUN:
-        case prog.S_HALT:
-            continue;
-        case prog.S_OUTPUT:
-            if (r < 128)
-                std::cout << static_cast<char>(r) << std::flush;
-            else
-                std::cout << r << std::endl;
-            break;
-        case prog.S_INPUT:
-            input = *script++;
-            break;
-        }
-    }
+    // Must be able to step after jump (E) or jump after jump (H)
+    const char *run =
+        "NOT T T\n"  // T = true
+        "AND A T\n"  // T = A
+        "AND B T\n"  // T = A && B
+        "AND C T\n"  // T = A && B && C
+        "NOT T T\n"  // T = !(A && B && C)  <- hole ahead
+        "NOT J J\n"  // J = true
+        "AND E J\n"  // J = E
+        "OR H J\n"   // J = E || H
+        "AND T J\n"  // J = T && (E || H)
+        "AND D J\n"  // J = T && (E || H) && D
+        "RUN\n";
+    MESSAGE(Survey(run, prog));
 }
