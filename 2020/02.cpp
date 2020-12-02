0034c12e3b015e447dac23d96c10227cc6cfda25
Anatolii Sakhnik <sakhnik@gmail.com>
2020-12-02 14:55:11 +0200

    2020-02.2

--- 2020/02.cpp
+++ 2020/02.cpp
@@ -4,7 +4,9 @@ namespace {
 
 using namespace boost::ut;
 
-bool IsValid(const std::string &pass)
+using ChecksT = std::pair<bool,bool>;
+
+ChecksT IsValid(const std::string &pass)
 {
     const static std::regex r{"([0-9]+)-([0-9]+) (.): (.+)"};
     std::smatch m;
@@ -14,29 +16,42 @@ bool IsValid(const std::string &pass)
     int max = std::stoi(m[2]);
     char c = *m[3].first;
 
+    ChecksT checks{};
+
     int count = std::count(m[4].first, m[4].second, c);
-    return count >= min && count <= max;
+    checks.first = count >= min && count <= max;
+
+    bool hasFirst = min <= m[4].length() && m[4].first[min - 1] == c;
+    bool hasSecond = max <= m[4].length() && m[4].first[max - 1] == c;
+    checks.second = hasFirst != hasSecond;
+
+    return checks;
 }
 
-int CountValid(std::istream &&is)
+using CountsT = std::pair<int,int>;
+
+CountsT CountValid(std::istream &&is)
 {
-    int count{};
+    CountsT counts{};
     std::string line;
     while (getline(is, line))
     {
-        if (IsValid(line))
-            ++count;
+        auto is_valid = IsValid(line);
+        counts.first += is_valid.first;
+        counts.second += is_valid.second;
     }
-    return count;
+    return counts;
 }
 
 suite s = [] {
     "2020-02"_test = [] {
-        expect(IsValid("1-3 a: abcde"));
-        expect(!IsValid("1-3 b: cdefg"));
-        expect(IsValid("2-9 c: ccccccccc"));
+        expect(IsValid("1-3 a: abcde") == ChecksT{true, true});
+        expect(IsValid("1-3 b: cdefg") == ChecksT{false, false});
+        expect(IsValid("2-9 c: ccccccccc") == ChecksT{true, false});
 
-        Printer::Print(__FILE__, "1", CountValid(std::ifstream{INPUT}));
+        auto counts = CountValid(std::ifstream{INPUT});
+        Printer::Print(__FILE__, "1", counts.first);
+        Printer::Print(__FILE__, "2", counts.second);
     };
 };
 
