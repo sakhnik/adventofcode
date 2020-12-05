c1fca064bcfbbe5eef05a6295e03c6129efd0adf
Anatolii Sakhnik <sakhnik@gmail.com>
2020-12-05 08:08:47 +0200

    2020-05.2

--- 2020/05.cpp
+++ 2020/05.cpp
@@ -1,46 +1,67 @@
 #include "../test.hpp"
 #include <fstream>
+#include <set>
+#include <algorithm>
 
 namespace {
 
 using namespace boost::ut;
 
-unsigned Parse(const std::string &id)
+class Seats
 {
-    unsigned code{};
-    for (size_t i = 0; i < id.size(); ++i)
+public:
+    Seats(std::istream &&is)
     {
-        code <<= 1;
-        const std::string_view upper{"BR"};
-        if (upper.find(id[i]) != upper.npos)
+        std::string id;
+        while (getline(is, id))
         {
-            code |= 1;
+            _occupied.insert(Parse(id));
         }
     }
-    return code;
-}
 
-unsigned FindHighest(std::istream &&is)
-{
-    unsigned highest_code{};
-    std::string id;
-    while (getline(is, id))
+    static unsigned Parse(const std::string &id)
     {
-        unsigned code = Parse(id);
-        if (highest_code < code)
-            highest_code = code;
+        unsigned code{};
+        for (size_t i = 0; i < id.size(); ++i)
+        {
+            code <<= 1;
+            const std::string_view upper{"BR"};
+            if (upper.find(id[i]) != upper.npos)
+            {
+                code |= 1;
+            }
+        }
+        return code;
     }
-    return highest_code;
-}
+
+    unsigned FindHighest() const
+    {
+        return *_occupied.rbegin();
+    }
+
+    unsigned FindGap() const
+    {
+        auto it = std::adjacent_find(_occupied.begin(), _occupied.end(),
+                                     [](unsigned a, unsigned b) {
+                                        return a + 1 != b;
+                                     });
+        return *it + 1;
+    }
+
+private:
+    std::set<unsigned> _occupied;
+};
 
 suite s = [] {
     "2020-05"_test = [] {
-        expect(357_u == Parse("FBFBBFFRLR"));
-        expect(567_u == Parse("BFFFBBFRRR"));
-        expect(119_u == Parse("FFFBBBFRRR"));
-        expect(820_u == Parse("BBFFBBFRLL"));
+        expect(357_u == Seats::Parse("FBFBBFFRLR"));
+        expect(567_u == Seats::Parse("BFFFBBFRRR"));
+        expect(119_u == Seats::Parse("FFFBBBFRRR"));
+        expect(820_u == Seats::Parse("BBFFBBFRLL"));
 
-        Printer::Print(__FILE__, "1", FindHighest(std::ifstream{INPUT}));
+        Seats seats{std::ifstream{INPUT}};
+        Printer::Print(__FILE__, "1", seats.FindHighest());
+        Printer::Print(__FILE__, "2", seats.FindGap());
     };
 };
 
