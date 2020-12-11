732cb4205609717befe3dc91ffa3cd777651615f
Anatolii Sakhnik <sakhnik@gmail.com>
2020-12-11 13:08:52 +0200

    2020-11.2

--- 2020/11.cpp
+++ 2020/11.cpp
@@ -18,7 +18,36 @@ public:
         _rows = (_seats.size() + 1) / (_cols + 1);
     }
 
+    Seats(const Seats &s)
+        : _seats{s._seats}
+        , _cols{s._cols}
+        , _rows{s._rows}
+    {
+    }
+
     void Run(int count = std::numeric_limits<int>::max())
+    {
+        _Run(count, [this](int r, int c) { return _CountNeighbours(r, c); }, 4);
+    }
+
+    void Run2(int count = std::numeric_limits<int>::max())
+    {
+        _Run(count, [this](int r, int c) { return _CountDirections(r, c); }, 5);
+    }
+
+    size_t CountOccupied() const
+    {
+        return std::count(_seats.begin(), _seats.end(), '#');
+    }
+
+    const std::string& GetSeats() const { return _seats; }
+
+private:
+    std::string _seats;
+    int _cols;
+    int _rows;
+
+    void _Run(int count, std::function<int(int, int)> counter, int tolerance)
     {
         do
         {
@@ -30,7 +59,7 @@ public:
                 for (int col = 0; col < _cols; ++col)
                 {
                     char cur = _Get(row, col);
-                    int neighbours = _CountNeighbours(row, col);
+                    int neighbours = counter(row, col);
                     if (cur == 'L')
                     {
                         if (neighbours == 0)
@@ -41,7 +70,7 @@ public:
                     }
                     else if (cur == '#')
                     {
-                        if (neighbours >= 4)
+                        if (neighbours >= tolerance)
                         {
                             buf[row * (_cols + 1) + col] = 'L';
                             change = true;
@@ -57,19 +86,6 @@ public:
         }
         while (--count > 0);
     }
-
-    size_t CountOccupied() const
-    {
-        return std::count(_seats.begin(), _seats.end(), '#');
-    }
-
-    const std::string& GetSeats() const { return _seats; }
-
-private:
-    std::string _seats;
-    int _cols;
-    int _rows;
-
     int _CountNeighbours(int row, int col) const
     {
         return (_Get(row - 1, col - 1) == '#')
@@ -80,6 +96,32 @@ private:
             + (_Get(row + 1, col - 1) == '#')
             + (_Get(row + 1, col + 0) == '#')
             + (_Get(row + 1, col + 1) == '#');
+    }
+
+    int _CountDirections(int row, int col) const
+    {
+        int count{};
+        for (int dr = -1; dr <= 1; ++dr)
+        {
+            for (int dc = -1; dc <= 1; ++dc)
+            {
+                if (dr || dc)
+                    count += _CountOneDirection(row, col, dr, dc);
+            }
+        }
+        return count;
+    }
+
+    int _CountOneDirection(int row, int col, int dr, int dc) const
+    {
+        row += dr;
+        col += dc;
+        switch (_Get(row, col))
+        {
+        case 'L': return 0;
+        case '#': return 1;
+        }
+        return _CountOneDirection(row, col, dr, dc);
     }
 
     char _Get(int row, int col) const
@@ -184,9 +226,57 @@ suite s = [] {
             expect(37_u == seats.CountOccupied());
         }
 
+        {
+            const char *const TEST =
+                "L.LL.LL.LL\n"
+                "LLLLLLL.LL\n"
+                "L.L.L..L..\n"
+                "LLLL.LL.LL\n"
+                "L.LL.LL.LL\n"
+                "L.LLLLL.LL\n"
+                "..L.L.....\n"
+                "LLLLLLLLLL\n"
+                "L.LLLLLL.L\n"
+                "L.LLLLL.LL";
+            Seats s{std::istringstream{TEST}};
+
+            s.Run2(1);
+            const std::string ROUND1 =
+                "#.##.##.##\n"
+                "#######.##\n"
+                "#.#.#..#..\n"
+                "####.##.##\n"
+                "#.##.##.##\n"
+                "#.#####.##\n"
+                "..#.#.....\n"
+                "##########\n"
+                "#.######.#\n"
+                "#.#####.##";
+            expect(eq(s.GetSeats(), ROUND1));
+
+            s.Run2(1);
+            const std::string ROUND2 =
+                "#.LL.LL.L#\n"
+                "#LLLLLL.LL\n"
+                "L.L.L..L..\n"
+                "LLLL.LL.LL\n"
+                "L.LL.LL.LL\n"
+                "L.LLLLL.LL\n"
+                "..L.L.....\n"
+                "LLLLLLLLL#\n"
+                "#.LLLLLL.L\n"
+                "#.LLLLL.L#";
+            expect(eq(s.GetSeats(), ROUND2));
+        }
+
         Seats seats{std::ifstream{INPUT}};
-        seats.Run(70);
+        Seats seats2{seats};
+
+        seats.Run();
         Printer::Print(__FILE__, "1", seats.CountOccupied());
+
+        seats2.Run2();
+        Printer::Print(__FILE__, "2", seats2.CountOccupied());
     };
 };
 
