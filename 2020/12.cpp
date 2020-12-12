6cee597b130c7fa28b8b84f638fabdfe3788fd3b
Anatolii Sakhnik <sakhnik@gmail.com>
2020-12-12 08:12:14 +0200

    2020-12.2

--- 2020/12.cpp
+++ 2020/12.cpp
@@ -45,14 +45,55 @@ int Track(std::istream &&is)
     return std::abs(x) + std::abs(y);
 }
 
+int Track2(std::istream &&is)
+{
+    int x{}, y{};
+    int wx{10}, wy{1};
+
+    char cmd{};
+    int value{};
+    while (is && (is >> cmd >> value))
+    {
+        switch (cmd)
+        {
+        case 'E': wx += value; break;
+        case 'S': wy -= value; break;
+        case 'W': wx -= value; break;
+        case 'N': wy += value; break;
+        case 'L':
+            for (; value > 0; value -= 90)
+            {
+                std::swap(wx, wy);
+                wx = -wx;
+            }
+            break;
+        case 'R':
+            for (; value > 0; value -= 90)
+            {
+                std::swap(wx, wy);
+                wy = -wy;
+            }
+            break;
+        case 'F':
+            x += wx * value;
+            y += wy * value;
+            break;
+        }
+    }
+
+    return std::abs(x) + std::abs(y);
+}
+
 using namespace boost::ut;
 
 suite s = [] {
     "2020-12"_test = [] {
         const char *const TEST = "F10\nN3\nF7\nR90\nF11";
         expect(25_i == Track(std::istringstream{TEST}));
+        expect(286_i == Track2(std::istringstream{TEST}));
 
         Printer::Print(__FILE__, "1", Track(std::ifstream{INPUT}));
+        Printer::Print(__FILE__, "2", Track2(std::ifstream{INPUT}));
     };
 };
 
