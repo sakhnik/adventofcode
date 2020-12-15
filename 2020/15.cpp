b3cb4122354accac2b1a2296623d5c2ecbdbc544
Anatolii Sakhnik <sakhnik@gmail.com>
2020-12-15 12:49:19 +0200

    2020-15.2

--- 2020/15.cpp
+++ 2020/15.cpp
@@ -11,7 +11,7 @@ public:
     {
         for (size_t turn = 0; turn != start.size(); ++turn)
         {
-            _history[start[turn]] = {turn};
+            _history[start[turn]] = _Info{turn, 0};
         }
         _turn = start.size();
         _recent = start.back();
@@ -21,16 +21,12 @@ public:
     {
         while (_turn < target)
         {
-            if (_history[_recent].size() < 2)
-            {
-                _recent = 0;
-            }
-            else
-            {
-                const auto &turns = _history[_recent];
-                _recent = turns[turns.size() - 1] - turns[turns.size() - 2];
-            }
-            _history[_recent].push_back(_turn);
+            const auto &lookup = _history[_recent];
+            _recent = lookup.interval;
+
+            auto &new_info = _history[_recent];
+            new_info.interval = new_info.prev_turn != _Info::NO_PREV ? _turn - new_info.prev_turn : 0;
+            new_info.prev_turn = _turn;
             ++_turn;
         }
         return _recent;
@@ -39,8 +35,14 @@ public:
 private:
     int _recent;
     size_t _turn;
-    // number -> turns
-    std::unordered_map<int, std::vector<size_t>> _history;
+    struct _Info
+    {
+        const static size_t NO_PREV = std::numeric_limits<size_t>::max();
+        size_t prev_turn = NO_PREV;  // Most recent turn the number appeared
+        int interval = 0;            // Interval to the previous recent turn
+    };
+    // number -> number info
+    std::unordered_map<int, _Info> _history;
 };
 
 using namespace boost::ut;
@@ -56,6 +58,17 @@ suite s = [] {
         expect(1836_i == Game{{3,1,2}}.Run(2020));
 
         Printer::Print(__FILE__, "1", Game{{9,12,1,4,17,0,18}}.Run(2020));
+
+        const int TARGET2 = 30000000;
+        //expect(175594_i == Game{{0,3,6}}.Run(TARGET2));
+        //expect(2578_i == Game{{1,3,2}}.Run(TARGET2));
+        //expect(3544142_i == Game{{2,1,3}}.Run(TARGET2));
+        //expect(261214_i == Game{{1,2,3}}.Run(TARGET2));
+        //expect(6895259_i == Game{{2,3,1}}.Run(TARGET2));
+        //expect(18_i == Game{{3,2,1}}.Run(TARGET2));
+        //expect(362_i == Game{{3,1,2}}.Run(TARGET2));
+
+        Printer::Print(__FILE__, "2", Game{{9,12,1,4,17,0,18}}.Run(TARGET2));
     };
 };
 
