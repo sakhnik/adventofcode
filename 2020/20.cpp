1561ba9b01d956f3bd6dcdee540e44b13b87bd89
Anatolii Sakhnik <sakhnik@gmail.com>
2020-12-20 21:59:50 +0200

    Remember the whole tile

--- 2020/20.cpp
+++ 2020/20.cpp
@@ -1,4 +1,6 @@
 #include "../test.hpp"
+#include <fstream>
+#include <unordered_map>
 
 namespace {
 
@@ -8,7 +10,6 @@ public:
     Tiles(std::istream &&is)
     {
         _Tile tile;
-        int row{};
 
         std::string line;
         while (getline(is, line))
@@ -17,24 +18,11 @@ public:
             {
                 _tiles.push_back(std::move(tile));
                 tile = _Tile{};
-                row = 0;
                 continue;
             }
             if (1 == sscanf(line.c_str(), "Tile %ld", &tile.id))
                 continue;
-            if (row == 0)
-            {
-                for (size_t i = 0; i < N; ++i)
-                    tile.e[0][i] = line[i] == '#';
-            }
-            else if (row == N - 1)
-            {
-                for (size_t i = 0; i < N; ++i)
-                    tile.e[2][i] = line[N - i - 1] == '#';
-            }
-            tile.e[1][row] = line.back() == '#';
-            tile.e[3][N - row - 1] = line.front() == '#';
-            ++row;
+            tile.rows.push_back(line);
         }
         _tiles.push_back(std::move(tile));
     }
@@ -42,34 +30,25 @@ public:
     int64_t FindCorners()
     {
         // Find tiles with two unique edges.
-        std::vector<std::vector<int64_t>> edge2id(1 << N);
+        std::unordered_map<std::string, std::vector<int64_t>> edge2id;
 
         for (const auto &tile : _tiles)
         {
             for (int i = 0; i < 4; ++i)
             {
-                edge2id[_CalcEdge(tile.e[i])].push_back(tile.id);
-                edge2id[_CalcEdge2(tile.e[i])].push_back(tile.id);
+                edge2id[tile.GetEdge(i, false)].push_back(tile.id);
+                edge2id[tile.GetEdge(i, true)].push_back(tile.id);
             }
         }
 
-        auto it = std::remove_if(begin(edge2id), end(edge2id), [](const auto &a) { return a.empty(); });
-        edge2id.erase(it, end(edge2id));
-
-        std::sort(begin(edge2id), end(edge2id), [](const auto &a, const auto &b) {
-                    return a.size() < b.size();
-                  });
-        //for (const auto &a : edge2id)
-        //{
-        //    std::cout << " " << a.size();
-        //}
-        //std::cout << std::endl;
-
         std::unordered_map<int64_t, int> counts;
 
-        for (size_t i = 0; i < edge2id.size() && edge2id[i].size() == 1; ++i)
+        for (const auto &edge_ids : edge2id)
         {
-            for (auto id : edge2id[i])
+            const auto &ids = edge_ids.second;
+            if (ids.size() != 1)
+                continue;
+            for (auto id : ids)
             {
                 ++counts[id];
             }
@@ -89,45 +68,39 @@ public:
 
 private:
     const static size_t N = 10;
-    using _EdgeT = std::array<bool, N>;
+
     struct _Tile
     {
         int64_t id;
-        std::array<_EdgeT, 4> e;
+        std::vector<std::string> rows;
 
-        _Tile Rotate(int c) const
+        std::string GetEdge(int c, bool flip) const
         {
-            _Tile t = *this;
-            std::rotate(begin(t.e), begin(t.e) + c, end(t.e));
-            return t;
-        }
-
-        _Tile Flip(int c) const
-        {
-            _Tile t = *this;
-            std::swap(t.e[c], t.e[c+2]);
-            std::reverse(t.e[c+1].begin(), t.e[c+1].end());
-            std::reverse(t.e[(c+3) % 4].begin(), t.e[(c+3) % 4].end());
-            return t;
+            std::string edge;
+            switch (c)
+            {
+            case 0:
+                for (size_t i = 0; i < N; ++i)
+                    edge.push_back(rows[0][i]);
+                break;
+            case 1:
+                for (size_t i = 0; i < N; ++i)
+                    edge.push_back(rows[i][N-1]);
+                break;
+            case 2:
+                for (size_t i = 0; i < N; ++i)
+                    edge.push_back(rows[N-1][N-i-1]);
+                break;
+            case 3:
+                for (size_t i = 0; i < N; ++i)
+                    edge.push_back(rows[N-i-1][0]);
+                break;
+            }
+            if (flip)
+                std::reverse(edge.begin(), edge.end());
+            return edge;
         }
     };
-
-    static int _CalcEdge(const _EdgeT &edge)
-    {
-        int res{};
-        for (size_t i = 0; i < edge.size(); ++i)
-        {
-            res <<= 1;
-            res |= edge[i];
-        }
-        return res;
-    };
-
-    static int _CalcEdge2(_EdgeT edge)
-    {
-        std::reverse(edge.begin(), edge.end());
-        return _CalcEdge(edge);
-    }
 
     std::vector<_Tile> _tiles;
 };
@@ -243,8 +216,7 @@ Tile 3079:
 #.#####.##
 ..#.###...
 ..#.......
-..#.###...
-            )";
+..#.###...)";
             Tiles tiles{std::istringstream{TEST}};
             expect(eq(20899048083289ll, tiles.FindCorners()));
         }
