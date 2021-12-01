b23e4613a83c6dd66e4f44be2f6a36f7ec16f7a4
Anatolii Sakhnik <sakhnik@gmail.com>
2021-12-01 10:52:27 +0200

    2021-01 part 2

--- 2021/01.cpp
+++ 2021/01.cpp
@@ -3,10 +3,24 @@
 namespace {
 
 template <typename IterT>
-int CountIncreased(IterT first, IterT last)
+int CountIncreased(int window_size, IterT first, IterT last)
 {
+    int sum{};  // Sum of depths in the sliding window
+    IterT i{first};  // Sliding window end iterator
+    // Preroll
+    while (window_size-- && i != last)
+        sum += *i++;
+
+    int prev_sum = sum;
     int count{};
-    std::adjacent_find(first, last, [&count](int a, int b) { if (a < b) { ++count; } return false; });
+    while (i != last)
+    {
+        // Update the sliding window sum
+        sum += *i++ - *first++;
+        if (sum > prev_sum)
+            ++count;
+        prev_sum = sum;
+    }
     return count;
 }
 
@@ -16,11 +30,14 @@ using namespace boost::ut;
 
 suite s = [] {
     "2021-01"_test = [] {
-        expect(7_i == CountIncreased(std::begin(TEST_DEPTHS), std::end(TEST_DEPTHS)));
+        expect(7_i == CountIncreased(1, std::begin(TEST_DEPTHS), std::end(TEST_DEPTHS)));
+        expect(5_i == CountIncreased(3, std::begin(TEST_DEPTHS), std::end(TEST_DEPTHS)));
 
         std::ifstream ifs{INPUT};
         std::istream_iterator<int> first{ifs}, last;
-        Printer::Print(__FILE__, "1", CountIncreased(first, last));
+        std::vector<int> depths{first, last};
+        Printer::Print(__FILE__, "1", CountIncreased(1, depths.begin(), depths.end()));
+        Printer::Print(__FILE__, "2", CountIncreased(3, depths.begin(), depths.end()));
     };
 };
 
