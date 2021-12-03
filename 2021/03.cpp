eddffa3c2f84bf473e126f666a39d4cb14e25432
Anatolii Sakhnik <sakhnik@gmail.com>
2021-12-03 13:01:00 +0200

    2020-03 part 2

--- 2021/03.cpp
+++ 2021/03.cpp
@@ -2,26 +2,74 @@
 
 namespace {
 
-size_t CalcConsumption(std::istream &&is)
+using NumbersT = std::vector<std::string>;
+
+NumbersT Parse(std::istream &&is)
 {
-    std::vector<int> counts;
-    size_t total{};
-    std::string s;
-    while (is >> s)
+    std::istream_iterator<std::string> first{is}, last{};
+    return NumbersT(first, last);
+}
+
+size_t CalcConsumption(const NumbersT &numbers)
+{
+    std::vector<int> counts(numbers[0].size());
+    for (const auto &number : numbers)
     {
-        if (counts.empty())
-            counts.resize(s.size());
-        for (int i = 0; i < s.size(); ++i)
-            counts[i] += s[i] - '0';
-        ++total;
+        for (int i = 0; i < number.size(); ++i)
+            counts[i] += number[i] - '0';
     }
     size_t gamma{}, epsilon{};
     for (int i = 0; i < counts.size(); ++i)
     {
-        gamma = (gamma << 1) + (counts[i] > total / 2);
-        epsilon = (epsilon << 1) + (counts[i] <= total / 2);
+        gamma = (gamma << 1) + (counts[i] > numbers.size() / 2);
+        epsilon = (epsilon << 1) + (counts[i] <= numbers.size() / 2);
     }
     return gamma * epsilon;
+}
+
+template <typename IterT, typename PredT>
+IterT Filter(IterT first, IterT last, int i, PredT pred)
+{
+    size_t count = last - first;
+    size_t ones = std::count_if(first, last, [i](const auto &n) { return n[i] == '1'; });
+    size_t zeros = count - ones;
+    return std::partition(first, last, [=](const auto &n) { return pred(n[i], zeros, ones); });
+}
+
+template <typename PredT>
+size_t Calc(NumbersT &numbers, PredT pred)
+{
+    auto it = numbers.end();
+    for (int i = 0; i < numbers[0].size(); ++i)
+    {
+        it = Filter(numbers.begin(), it, i, pred);
+        if (it == numbers.begin() + 1)
+        {
+            size_t val{};
+            for (int j = 0; j < numbers[0].size(); ++j)
+                val = (val << 1) + (numbers[0][j] - '0');
+            return val;
+        }
+    }
+    throw std::runtime_error("Filtering failed");
+}
+
+size_t CalcOxygen(NumbersT &numbers)
+{
+    return Calc(numbers, [](char ch, size_t zeros, size_t ones) {
+        if (zeros == ones)
+            return ch == '1';
+        return ch == (zeros > ones ? '0' : '1');
+    });
+}
+
+size_t CalcCarbonDioxide(NumbersT &numbers)
+{
+    return Calc(numbers, [](char ch, size_t zeros, size_t ones) {
+        if (zeros == ones)
+            return ch == '0';
+        return ch == (zeros > ones ? '1' : '0');
+    });
 }
 
 using namespace boost::ut;
@@ -41,8 +89,14 @@ const char *const TEST_INPUT = R"(00100
 
 suite s = [] {
     "2021-03"_test = [] {
-        expect(198_u == CalcConsumption(std::istringstream{TEST_INPUT}));
-        Printer::Print(__FILE__, "1", CalcConsumption(std::ifstream{INPUT}));
+        auto TEST_NUMBERS = Parse(std::istringstream{TEST_INPUT});
+        expect(198_u == CalcConsumption(TEST_NUMBERS));
+        auto NUMBERS = Parse(std::ifstream{INPUT});
+        Printer::Print(__FILE__, "1", CalcConsumption(NUMBERS));
+
+        expect(23_u == CalcOxygen(TEST_NUMBERS));
+        expect(10_u == CalcCarbonDioxide(TEST_NUMBERS));
+        Printer::Print(__FILE__, "2", CalcOxygen(NUMBERS) * CalcCarbonDioxide(NUMBERS));
     };
 };
 
