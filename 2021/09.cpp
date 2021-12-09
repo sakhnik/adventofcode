f63777be65f44569f01dccbf2034ffd1c9b2c4f7
Anatolii Sakhnik <sakhnik@gmail.com>
2021-12-09 10:08:23 +0200

    2021-09 part 2

--- 2021/09.cpp
+++ 2021/09.cpp
@@ -1,4 +1,5 @@
 #include "../test.hpp"
+#include <iterator>
 
 namespace {
 
@@ -33,6 +34,51 @@ public:
         return risk;
     }
 
+    size_t CalcBasins() const
+    {
+        int rows = _heights.size();
+        int cols = _heights[0].size();
+
+        auto idx = [cols](int row, int col) {
+            return row * cols + col;
+        };
+
+        std::vector<size_t> basins = {0};
+        std::vector<size_t> visited(idx(rows, 0), 0);
+
+        std::function<void(int, int, size_t)> visit;
+        visit = [&, this](int row, int col, size_t basin_idx) {
+            if (visited[idx(row, col)] || _heights[row][col] == '9')
+                return;
+            if (!basin_idx)
+            {
+                basins.push_back(0);
+                basin_idx = basins.size() - 1;
+            }
+            visited[idx(row, col)] = basin_idx;
+            ++basins[basin_idx];
+            if (row > 0)
+                visit(row - 1, col, basin_idx);
+            if (row < rows - 1)
+                visit(row + 1, col, basin_idx);
+            if (col > 0)
+                visit(row, col - 1, basin_idx);
+            if (col < cols - 1)
+                visit(row, col + 1, basin_idx);
+        };
+
+        for (int row = 0; row < rows; ++row)
+        {
+            for (int col = 0; col < cols; ++col)
+            {
+                visit(row, col, 0);
+            }
+        }
+
+        std::sort(basins.begin(), basins.end(), [](size_t a, size_t b) { return a > b; });
+        return basins[0] * basins[1] * basins[2];
+    }
+
 private:
     std::vector<std::string> _heights;
 };
@@ -51,9 +97,11 @@ suite s = [] {
     "2021-09"_test = [] {
         Map test_map{std::istringstream{TEST_INPUT}};
         expect(15_i == test_map.CalcRiskLevel());
+        expect(1134_u == test_map.CalcBasins());
 
         Map m{std::ifstream{INPUT}};
         Printer::Print(__FILE__, "1", m.CalcRiskLevel());
+        Printer::Print(__FILE__, "2", m.CalcBasins());
     };
 };
 
