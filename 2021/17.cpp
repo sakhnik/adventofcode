272deb644184c030cfe8cfc53475013fd0ca0c10
Anatolii Sakhnik <sakhnik@gmail.com>
2021-12-17 15:27:40 +0200

    2021-17 part 2

--- 2021/17.cpp
+++ 2021/17.cpp
@@ -1,4 +1,5 @@
 #include "../test.hpp"
+#include <boost/functional/hash.hpp>
 
 namespace {
 
@@ -47,6 +48,74 @@ public:
         return dy * (dy - 1) / 2;
     }
 
+    int CalcFallTime(int dy, int y) const
+    {
+        // y = v + (v - 1) + (v - 2) + (v - 3) + ... + (v - t) = vt - t(t - 1)/2
+        //
+        // y = vt - t²/2 + t/2
+        // 2y = 2vt - t² + t
+        //
+        // t² - (2v + 1)t + 2y = 0
+
+        double a = 1.0;
+        double b = -(2.0 * dy + 1);
+        double c = 2.0 * y;
+
+        double d = b * b - 4 * a * c;
+        if (d < 0)
+            return 0;
+        d = std::sqrt(d);
+        double x = (-b - d) / (a + a);
+        if (x > 0)
+        {
+            double t{};
+            if (std::modf(x, &t) < 1e-10)
+                return t;
+        }
+
+        x = (-b + d) / (a + a);
+        if (x < 0)
+            return 0;
+        double t{};
+        if (std::modf(x, &t) < 1e-10)
+            return t;
+        return 0;
+    }
+
+    int CalcX(int dx, int t) const
+    {
+        auto t0 = std::min(dx, t);
+        return (2 * dx - t0 + 1) * t0 / 2;
+    }
+
+    int CalcSpeeds() const
+    {
+        int dy_min = target_y.first;
+        int dy_max = -target_y.first;
+
+        using SpeedT = std::pair<int, int>;
+        std::unordered_set<SpeedT, boost::hash<SpeedT>> speeds;
+
+        for (int dy = dy_min; dy <= dy_max; ++dy)
+        {
+            for (int y = target_y.first; y <= target_y.second; ++y)
+            {
+                int t = CalcFallTime(dy, y);
+                if (!t)
+                    continue;
+                for (int dx = 5; dx <= target_x.second; ++dx)
+                {
+                    auto x = CalcX(dx, t);
+                    if (x >= target_x.first && x <= target_x.second)
+                    {
+                        speeds.insert({dx, dy});
+                    }
+                }
+            }
+        }
+        return speeds.size();
+    }
+
     using RangeT = std::pair<int, int>;
     RangeT target_x{};
     RangeT target_y{};
@@ -66,9 +135,17 @@ suite s = [] {
         expect(Probe::MISS == test.Shoot(17, -4));
         expect(45_i == test.Shoot(6, 9));
         expect(45_i == test.FindHighest());
+        expect(6_i == test.CalcFallTime(2, -3));
+        expect(0_i == test.CalcFallTime(-4, -6));
+        expect(2_i == test.CalcX(2, 1));
+        expect(3_i == test.CalcX(2, 2));
+        expect(3_i == test.CalcX(2, 4));
+        expect(3_i == test.CalcX(2, 5));
+        expect(112_i == test.CalcSpeeds());
 
         Probe probe{std::ifstream{INPUT}};
         Printer::Print(__FILE__, "1", probe.FindHighest());
+        Printer::Print(__FILE__, "2", probe.CalcSpeeds());
     };
 };
 
