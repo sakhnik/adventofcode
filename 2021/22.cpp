680320e9d8077f4ee7435e4224529a70821747f7
Anatolii Sakhnik <sakhnik@gmail.com>
2021-12-24 18:09:07 +0200

    2021-22 part 2

--- 2021/22.cpp
+++ 2021/22.cpp
@@ -9,21 +9,13 @@ struct Cube
 {
     PointT p1;
     PointT p2;
+    int sign = 1;
 
-    Cube(PointT p1, PointT p2)
+    Cube(PointT p1, PointT p2, int sign = 1)
         : p1{p1}
         , p2{p2}
+        , sign{sign}
     {
-    }
-
-    bool operator==(const Cube &o) const
-    {
-        return p1 == o.p1 && p2 == o.p2;
-    }
-
-    bool operator<(const Cube &o) const
-    {
-        return p1 < o.p1 || (p1 == o.p1 && p2 < o.p2);
     }
 
     bool Intersects(const Cube &o) const
@@ -42,153 +34,31 @@ struct Cube
         return true;
     }
 
-    bool Crosses(const Cube &o) const
+    Cube GetIntersection(const Cube &o) const
     {
-        if (!Intersects(o))
-            return false;
+        Cube ret{{},{}};
         for (int i = 0; i < 3; ++i)
         {
-            if ((p1[i] < o.p1[i] && o.p1[i] < p2[i]) || (p1[i] < o.p2[i] && o.p2[i] < p2[i]))
-                return true;
+            ret.p1[i] = std::max(p1[i], o.p1[i]);
+            ret.p2[i] = std::min(p2[i], o.p2[i]);
         }
-        return false;
+        ret.sign = -o.sign;
+        return ret;
     }
 
-    std::list<Cube> Slice(const Cube &o) const
+    ssize_t Volume() const
     {
-        std::list<Cube> cubes{*this};
-        for (int i = 0; i < 3; ++i)
-        {
-            for (int p : {o.p1[i], o.p2[i]})
-            {
-                for (auto it = cubes.begin(); it != cubes.end(); )
-                {
-                    if (it->p1[i] < p && it->p2[i] > p)
-                    {
-                        auto to_split = *it;
-                        it = cubes.erase(it);
-                        PointT pm1 = to_split.p2;
-                        pm1[i] = p;
-                        it = cubes.insert(it, Cube{to_split.p1, pm1});
-                        PointT pm2 = to_split.p1;
-                        pm2[i] = p;
-                        it = cubes.insert(++it, Cube{pm2, to_split.p2});
-                    }
-                    ++it;
-                }
-            }
-        }
-        return cubes;
-    }
-
-    size_t Volume() const
-    {
-        size_t v{1};
+        ssize_t v{sign};
         for (int i = 0; i < 3; ++i)
             v *= p2[i] - p1[i];
         return v;
     }
 };
 
-std::ostream& operator<<(std::ostream &os, const PointT &p)
-{
-    return os << p[0] << "," << p[1] << "," << p[2];
-}
-
-std::ostream& operator<<(std::ostream &os, const Cube &c)
-{
-    return os << c.p1 << ":" << c.p2;
-}
-
-struct Geometry
-{
-    using CubesT = std::list<Cube>;
-    CubesT cubes;
-
-    Geometry(CubesT &&cubes)
-        : cubes{cubes}
-    {
-    }
-
-    size_t size() const { return cubes.size(); }
-
-    size_t Volume() const
-    {
-        size_t v{};
-        for (const auto &c : cubes)
-            v += c.Volume();
-        return v;
-    }
-
-    void Reduce()
-    {
-        cubes.sort();
-        for (auto it2 = cubes.begin(), it1{it2++}; it2 != cubes.end(); ++it2)
-        {
-            if (*it1 == *it2)
-                it1 = cubes.erase(it1);
-            else
-                ++it1;
-        }
-    }
-
-    void Slice(const Cube &o)
-    {
-        for (auto it = cubes.begin(); it != cubes.end(); )
-        {
-            if (!it->Crosses(o))
-                ++it;
-            else
-            {
-                auto sl = it->Slice(o);
-                it = cubes.erase(it);
-                cubes.splice(it, sl);
-            }
-        }
-        //for (auto &c : cubes)
-        //    assert(!c.Crosses(o));
-    }
-
-    void Add(const Cube &o)
-    {
-        Slice(o);
-        Geometry g({o});
-        for (const auto &c : cubes)
-            g.Slice(c);
-        cubes.splice(cubes.end(), g.cubes);
-        Reduce();
-    }
-
-    void Remove(const Cube &o)
-    {
-        Slice(o);
-        for (auto it = cubes.begin(); it != cubes.end(); )
-        {
-            if (it->Intersects(o))
-                it = cubes.erase(it);
-            else
-                ++it;
-        }
-    }
-
-    void Intersect(const Cube &o)
-    {
-        Slice(o);
-        for (auto it = cubes.begin(); it != cubes.end(); )
-        {
-            if (!it->Intersects(o))
-                it = cubes.erase(it);
-            else
-                ++it;
-        }
-    }
-};
-
 class Reactor
 {
 public:
-    using CmdT = std::pair<std::string, Cube>;
-    using CmdsT = std::vector<CmdT>;
+    using CmdsT = std::vector<Cube>;
     CmdsT cmds;
 
     Reactor(std::istream &&is)
@@ -204,45 +74,28 @@ public:
             {
                 for (auto &p : c.p2)
                     ++p;
-                cmds.emplace_back(cmd, c);
+                c.sign = std::string_view{cmd} == "on" ? 1 : -1;
+                cmds.emplace_back(c);
             }
         }
-    }
-
-    size_t Execute(const Cube &core, size_t max_count = std::numeric_limits<size_t>::max()) const
-    {
-        Geometry reactor{{}};
-        for (const auto &[cmd, cube] : cmds)
-        {
-            if (cmd == "on")
-            {
-                reactor.Add(cube);
-                reactor.Intersect(core);
-            }
-            else if (cmd == "off")
-                reactor.Remove(cube);
-            if (!--max_count)
-                break;
-        }
-        return reactor.Volume();
     }
 
     size_t ExecuteTest(const Cube &core, size_t max_count = std::numeric_limits<size_t>::max()) const
     {
         std::unordered_set<PointT, boost::hash<PointT>> points;
 
-        for (const auto &[cmd, cube] : cmds)
+        for (const auto &cmd : cmds)
         {
-            for (int x = std::max(cube.p1[0], core.p1[0]), x_end = std::min(cube.p2[0], core.p2[0]);
+            for (int x = std::max(cmd.p1[0], core.p1[0]), x_end = std::min(cmd.p2[0], core.p2[0]);
                     x < x_end; ++x)
             {
-                for (int y = std::max(cube.p1[1], core.p1[1]), y_end = std::min(cube.p2[1], core.p2[1]);
+                for (int y = std::max(cmd.p1[1], core.p1[1]), y_end = std::min(cmd.p2[1], core.p2[1]);
                         y < y_end; ++y)
                 {
-                    for (int z = std::max(cube.p1[2], core.p1[2]), z_end = std::min(cube.p2[2], core.p2[2]);
+                    for (int z = std::max(cmd.p1[2], core.p1[2]), z_end = std::min(cmd.p2[2], core.p2[2]);
                             z < z_end; ++z)
                     {
-                        if (cmd == "on")
+                        if (cmd.sign > 0)
                             points.insert({x,y,z});
                         else
                             points.erase({x,y,z});
@@ -254,6 +107,26 @@ public:
                 break;
         }
         return points.size();
+    }
+
+    size_t Execute() const
+    {
+        std::vector<Cube> cubes;
+
+        for (const auto &cmd : cmds)
+        {
+            std::vector<Cube> intersections;
+            for (const auto &c : cubes)
+            {
+                if (c.Intersects(cmd))
+                    intersections.push_back(cmd.GetIntersection(c));
+            }
+            cubes.insert(cubes.end(), intersections.begin(), intersections.end());
+            if (cmd.sign > 0)
+                cubes.push_back(cmd);
+        }
+
+        return std::accumulate(cubes.begin(), cubes.end(), 0ll, [](ssize_t v, const Cube &c) { return v += c.Volume(); });
     }
 };
 
@@ -282,6 +155,69 @@ on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
 on x=967..23432,y=45373..81175,z=27513..53682
 )";
 
+const char *const TEST2 = R"(
+on x=-5..47,y=-31..22,z=-19..33
+on x=-44..5,y=-27..21,z=-14..35
+on x=-49..-1,y=-11..42,z=-10..38
+on x=-20..34,y=-40..6,z=-44..1
+off x=26..39,y=40..50,z=-2..11
+on x=-41..5,y=-41..6,z=-36..8
+off x=-43..-33,y=-45..-28,z=7..25
+on x=-33..15,y=-32..19,z=-34..11
+off x=35..47,y=-46..-34,z=-11..5
+on x=-14..36,y=-6..44,z=-16..29
+on x=-57795..-6158,y=29564..72030,z=20435..90618
+on x=36731..105352,y=-21140..28532,z=16094..90401
+on x=30999..107136,y=-53464..15513,z=8553..71215
+on x=13528..83982,y=-99403..-27377,z=-24141..23996
+on x=-72682..-12347,y=18159..111354,z=7391..80950
+on x=-1060..80757,y=-65301..-20884,z=-103788..-16709
+on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856
+on x=-52752..22273,y=-49450..9096,z=54442..119054
+on x=-29982..40483,y=-108474..-28371,z=-24328..38471
+on x=-4958..62750,y=40422..118853,z=-7672..65583
+on x=55694..108686,y=-43367..46958,z=-26781..48729
+on x=-98497..-18186,y=-63569..3412,z=1232..88485
+on x=-726..56291,y=-62629..13224,z=18033..85226
+on x=-110886..-34664,y=-81338..-8658,z=8914..63723
+on x=-55829..24974,y=-16897..54165,z=-121762..-28058
+on x=-65152..-11147,y=22489..91432,z=-58782..1780
+on x=-120100..-32970,y=-46592..27473,z=-11695..61039
+on x=-18631..37533,y=-124565..-50804,z=-35667..28308
+on x=-57817..18248,y=49321..117703,z=5745..55881
+on x=14781..98692,y=-1341..70827,z=15753..70151
+on x=-34419..55919,y=-19626..40991,z=39015..114138
+on x=-60785..11593,y=-56135..2999,z=-95368..-26915
+on x=-32178..58085,y=17647..101866,z=-91405..-8878
+on x=-53655..12091,y=50097..105568,z=-75335..-4862
+on x=-111166..-40997,y=-71714..2688,z=5609..50954
+on x=-16602..70118,y=-98693..-44401,z=5197..76897
+on x=16383..101554,y=4615..83635,z=-44907..18747
+off x=-95822..-15171,y=-19987..48940,z=10804..104439
+on x=-89813..-14614,y=16069..88491,z=-3297..45228
+on x=41075..99376,y=-20427..49978,z=-52012..13762
+on x=-21330..50085,y=-17944..62733,z=-112280..-30197
+on x=-16478..35915,y=36008..118594,z=-7885..47086
+off x=-98156..-27851,y=-49952..43171,z=-99005..-8456
+off x=2032..69770,y=-71013..4824,z=7471..94418
+on x=43670..120875,y=-42068..12382,z=-24787..38892
+off x=37514..111226,y=-45862..25743,z=-16714..54663
+off x=25699..97951,y=-30668..59918,z=-15349..69697
+off x=-44271..17935,y=-9516..60759,z=49131..112598
+on x=-61695..-5813,y=40978..94975,z=8655..80240
+off x=-101086..-9439,y=-7088..67543,z=33935..83858
+off x=18020..114017,y=-48931..32606,z=21474..89843
+off x=-77139..10506,y=-89994..-18797,z=-80..59318
+off x=8476..79288,y=-75520..11602,z=-96624..-24783
+on x=-47488..-1262,y=24338..100707,z=16292..72967
+off x=-84341..13987,y=2429..92914,z=-90671..-1318
+off x=-37810..49457,y=-71013..-7894,z=-105357..-13188
+off x=-27365..46395,y=31009..98017,z=15428..76570
+off x=-70369..-16548,y=22648..78696,z=-1892..86821
+on x=-53470..21291,y=-120233..-33476,z=-44150..38147
+off x=-93533..-4276,y=-16170..68771,z=-104985..-24507
+)";
+
 using namespace boost::ut;
 using namespace std::string_literals;
 
@@ -293,44 +229,18 @@ suite s = [] {
             expect(8_u == c1.Volume());
             Cube c2{{3,3,3}, {4,4,4}};
             expect(!c1.Intersects(c2));
-
-            auto sl1 = c1.Slice(c1);
-            expect(1_u == sl1.size());
-            expect(eq(c1, sl1.front()));
-
-            Cube c3{{0,0,0}, {1,1,1}};
-            expect(c1.Intersects(c3));
-            Geometry sl2 = c1.Slice(c3);
-            expect(8_u == sl2.size());
-            expect(8_u == sl2.Volume());
-
-            Cube c4{{1,1,1}, {3,3,3}};
-            expect(c1.Intersects(c4));
-            Geometry sl3 = c1.Slice(c4);
-            expect(8_u == sl3.size());
-            expect(8_u == sl3.Volume());
-        }
-
-        {
-            Geometry g{{}};
-            g.Add({{10,10,10}, {13,13,13}});
-            expect(27_u == g.Volume());
-            g.Add({{11,11,11}, {14,14,14}});
-            expect(46_u == g.Volume());
-            g.Remove({{9,9,9}, {12,12,12}});
-            expect(38_u == g.Volume());
-            g.Add({{10,10,10}, {11,11,11}});
-            expect(39_u == g.Volume());
         }
 
         Cube test_core{{-50, -50, -50}, {51, 51, 51}};
+
         Reactor test{std::istringstream{TEST}};
-        //size_t count = 6;
-        //expect(eq(test.ExecuteTest(test_core, count), test.Execute(test_core, count)));
         expect(590784_u == test.ExecuteTest(test_core));
+        Reactor test2{std::istringstream{TEST2}};
+        expect(eq(2758514936282235ull, test2.Execute()));
 
         Reactor reactor{std::ifstream{INPUT}};
         Printer::Print(__FILE__, "1", reactor.ExecuteTest(test_core));
+        Printer::Print(__FILE__, "2", reactor.Execute());
     };
 };
 
