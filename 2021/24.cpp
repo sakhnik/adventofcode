a051a3dfaf7c20889ab64aaf01fb6632bfb7eed3
Anatolii Sakhnik <sakhnik@gmail.com>
2021-12-29 15:56:13 +0200

    2021-24

--- 2021/24.cpp
+++ 2021/24.cpp
@@ -1,5 +1,5 @@
 #include "../test.hpp"
-#include <boost/functional/hash.hpp>
+#include <boost/range/irange.hpp>
 
 namespace {
 
@@ -8,259 +8,6 @@ class Alu
 public:
     static int64_t Calc(const char *input)
     {
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 1
-        // add x 10
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 13
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 1
-        // add x 13
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 10
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 1
-        // add x 13
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 3
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 26
-        // add x -11
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 1
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 1
-        // add x 11
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 9
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 26
-        // add x -4
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 3
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 1
-        // add x 12
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 5
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 1
-        // add x 12
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 1
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 1
-        // add x 15
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 0
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 26
-        // add x -2
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 13
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 26
-        // add x -5
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 7
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 26
-        // add x -11
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 15
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 26
-        // add x -13
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 12
-        // mul y x
-        // add z y
-        // inp w
-        // mul x 0
-        // add x z
-        // mod x 26
-        // div z 26
-        // add x -10
-        // eql x w
-        // eql x 0
-        // mul y 0
-        // add y 25
-        // mul y x
-        // add y 1
-        // mul z y
-        // mul y 0
-        // add y w
-        // add y 8
-        // mul y x
-        // add z y
-
         // Translate into C++:
         // '<,'>s/inp \(.\)/\1 = *input++ - '0';/g
         // '<,'>s/mul \([^ ]\+\) \([^ ]\+\)/\1 *= \2;/g
@@ -359,162 +106,86 @@ public:
         return z;
     }
 
-    static const std::function<int64_t(int64_t, int)> STAGES[14];
+    std::vector<std::string> lines;
 
-    static int64_t CalcChained(const char *input)
+    Alu(std::istream &&is)
     {
-        int64_t z{};
-        for (auto f : STAGES)
-            z = f(z, *input++ - '0');
-        return z;
+        std::string line;
+        while (getline(is, line))
+            lines.push_back(line);
     }
 
-    //static std::string Solve()
-    //{
-    //    using ZW = std::pair<int64_t, int>;
-    //    using ValuesT = std::unordered_map<ZW, std::vector<int64_t>, boost::hash<ZW>>;
-    //    ValuesT values;
+    int GetArg(size_t i) const
+    {
+        const auto &line = lines[i];
+        auto last = line.find_last_of(" ");
+        return std::stoi(line.substr(last + 1));
+    }
 
-    //    for (int64_t z = 0; z < 1000000; ++z)
-    //        for (int w = 1; w <= 9; ++w)
-    //        {
-    //            int64_t zr = Stage14(z, w);
-    //            values[{zr, w}].push_back(z);
-    //        }
-    //    return "";
-    //}
+    std::string Find(bool biggest = true) const
+    {
+        const int COUNT = 14;
+        const auto N = lines.size() / COUNT;
+        std::string digits(COUNT, '0');
+        std::vector<std::pair<int, int>> st;
+
+        for (int i = 0; i < COUNT; i++)
+        {
+            auto off = i * N;
+            if (GetArg(off + 4) == 1)
+            {
+                auto add2 = GetArg(off + 15);
+                st.emplace_back(i, add2);
+                continue;
+            }
+
+            auto add1 = GetArg(off + 5);
+            auto [num, add2] = st.back();
+            st.pop_back();
+            auto delta = add1 + add2;
+            for (char digit : biggest ? boost::irange<char>('9', '1' - 1, -1) : boost::irange<char>('1', '9' + 1, 1))
+            {
+                auto digit2 = digit + delta;
+                if (digit2 >= '1' && digit2 <= '9')
+                {
+                    digits[num] = digit;
+                    digits[i] = digit2;
+                    break;
+                }
+            }
+        }
+        return digits;
+    }
 };
 
-const std::function<int64_t(int64_t, int)> Alu::STAGES[14] = {
-    [](int64_t z, int w) {
-        z = w + 13;
-        return z;
-    },
-    [](int64_t z, int w) {
-        z = z * 26 + w + 10;
-        return z;
-    },
-    [](int64_t z, int w) {
-        z = z * 26 + w + 3;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 - 11;
-        x = x != w;
-
-        z /= 26;
-        z *= 25 * x + 1;
-        z += x * (w + 1);
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 != w;
-        z *= 25 * x + 1;
-        z += (w + 9) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 - 4;
-        z /= 26;
-        x = x != w;
-        z *= 25 * x + 1;
-        z += (w + 3) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 + 12;
-        x = x != w;
-
-        z *= 25 * x + 1;
-        z += (w + 5) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 + 12;
-        x = x != w;
-
-        z *= 25 * x + 1;
-        z += (w + 1) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 + 15;
-        x = x != w;
-
-        z *= 25 * x + 1;
-        z += w * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 - 2;
-        x = x != w;
-
-        z /= 26;
-        z *= 25 * x + 1;
-        z += (w + 13) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 - 5;
-        x = x != w;
-
-        z /= 26;
-        z *= 25 * x + 1;
-        z += (w + 7) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 - 11;
-        x = x != w;
-
-        z /= 26;
-        z *= 25 * x + 1;
-        z += (w + 15) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = z % 26 - 13;
-        x = x != w;
-
-        z /= 26;
-        z *= 25 * x + 1;
-        z += (w + 12) * x;
-        return z;
-    },
-    [](int64_t z, int w) {
-        int64_t x = (z % 26 - 10) != w;
-        z /= 26;
-        z *= 25 * x + 1;
-        z += (w + 8) * x;
-        return z;
-    },
-};
 
 using namespace boost::ut;
 using namespace std::string_literals;
 
 suite s = [] {
     "2021-24"_test = [] {
-        expect(eq(4135231966ll, Alu::CalcChained("00000000000000")));
-        expect(eq(4135231967ll, Alu::CalcChained("00000000000001")));
-        expect(eq(159047383ll, Alu::CalcChained("00000000000002")));
-        expect(eq(4456504373ll, Alu::CalcChained("11111111111111")));
-        expect(eq(4777776780ll, Alu::CalcChained("22222222222222")));
-        expect(eq(5099049187ll, Alu::CalcChained("33333333333333")));
-        expect(eq(5420321594ll, Alu::CalcChained("44444444444444")));
-        expect(eq(5741594001ll, Alu::CalcChained("55555555555555")));
-        expect(eq(6062866408ll, Alu::CalcChained("66666666666666")));
-        expect(eq(6384138815ll, Alu::CalcChained("77777777777777")));
-        expect(eq(6705411222ll, Alu::CalcChained("88888888888888")));
-        expect(eq(7026683626ll, Alu::CalcChained("99999999999996")));
-        expect(eq(7026683627ll, Alu::CalcChained("99999999999997")));
-        expect(eq(7026683628ll, Alu::CalcChained("99999999999998")));
-        expect(eq(7026683629ll, Alu::CalcChained("99999999999999")));
+        expect(eq(4135231966ll, Alu::Calc("00000000000000")));
+        expect(eq(4135231967ll, Alu::Calc("00000000000001")));
+        expect(eq(159047383ll, Alu::Calc("00000000000002")));
+        expect(eq(4456504373ll, Alu::Calc("11111111111111")));
+        expect(eq(4777776780ll, Alu::Calc("22222222222222")));
+        expect(eq(5099049187ll, Alu::Calc("33333333333333")));
+        expect(eq(5420321594ll, Alu::Calc("44444444444444")));
+        expect(eq(5741594001ll, Alu::Calc("55555555555555")));
+        expect(eq(6062866408ll, Alu::Calc("66666666666666")));
+        expect(eq(6384138815ll, Alu::Calc("77777777777777")));
+        expect(eq(6705411222ll, Alu::Calc("88888888888888")));
+        expect(eq(7026683626ll, Alu::Calc("99999999999996")));
+        expect(eq(7026683627ll, Alu::Calc("99999999999997")));
+        expect(eq(7026683628ll, Alu::Calc("99999999999998")));
+        expect(eq(7026683629ll, Alu::Calc("99999999999999")));
 
- 
-        //Alu::Solve();
-        //Printer::Print(__FILE__, "1", Amphipods::Arrange(task));
-        //Printer::Print(__FILE__, "2", Amphipods::Arrange(task2));
+        Alu alu{std::ifstream{INPUT}};
+        expect(1 == alu.GetArg(4));
+        expect(10 == alu.GetArg(5));
+        expect(13 == alu.GetArg(15));
+        Printer::Print(__FILE__, "1", alu.Find(true));
+        Printer::Print(__FILE__, "2", alu.Find(false));
     };
 };
 
