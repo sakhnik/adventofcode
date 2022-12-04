368c9757b6e3cb03213475a3014749dfb282fa26
Anatolii Sakhnik <sakhnik@gmail.com>
2022-12-04 14:08:50 +0200

    2022-04.2

--- 2022/04.cpp
+++ 2022/04.cpp
@@ -11,10 +11,11 @@ const char *const TEST = R"(2-4,6-8
 2-6,4-8
 )";
 
-int CountOverlaps(std::istream &&is)
+std::pair<int, int> CountOverlaps(std::istream &&is)
 {
     using RangeT = std::pair<int, int>;
-    int count{};
+    int full_overlap{};
+    int any_overlap{};
 
     std::string line;
     while (std::getline(is, line))
@@ -23,24 +24,31 @@ int CountOverlaps(std::istream &&is)
         sscanf(line.c_str(), "%d-%d,%d-%d", &a.first, &a.second, &b.first, &b.second);
         if (a.first == b.first)
         {
-            ++count;
+            ++full_overlap;
+            ++any_overlap;
             continue;
         }
         if (a.first > b.first)
             a.swap(b);
         if (a.second >= b.second)
-            ++count;
+            ++full_overlap;
+        if (a.second >= b.first)
+            ++any_overlap;
     }
-    return count;
+    return {full_overlap, any_overlap};
 }
 
 using namespace boost::ut;
 
 suite s = [] {
     "2022-04"_test = [] {
-        expect(2_i == CountOverlaps(std::istringstream{TEST}));
+        auto test = CountOverlaps(std::istringstream{TEST});
+        expect(2_i == test.first);
+        expect(4_i == test.second);
 
-        Printer::Print(__FILE__, "1", CountOverlaps(std::ifstream{INPUT}));
+        auto res = CountOverlaps(std::ifstream{INPUT});
+        Printer::Print(__FILE__, "1", res.first);
+        Printer::Print(__FILE__, "2", res.second);
     };
 };
 
