9d2e7d27f0c8e8704bc9571957b837e9aaad7632
Anatolii Sakhnik <sakhnik@gmail.com>
2022-12-05 13:54:01 +0200

    2022-05.2

--- 2022/05.cpp
+++ 2022/05.cpp
@@ -15,9 +15,11 @@ move 2 from 2 to 1
 move 1 from 1 to 2
 )";
 
-std::string Rearrange(std::istream &&is)
+using StacksT = std::vector<std::string>;
+
+StacksT ReadStacks(std::istream &is)
 {
-    std::vector<std::string> stacks;
+    StacksT stacks;
 
     std::string line;
     while (std::getline(is, line))
@@ -39,7 +41,25 @@ std::string Rearrange(std::istream &&is)
 
     for (auto &s : stacks)
         std::reverse(s.begin(), s.end());
+    return stacks;
+}
 
+std::string GetTops(const StacksT &stacks)
+{
+    std::string tops;
+    for (auto &s : stacks)
+    {
+        if (!s.empty())
+            tops.push_back(s.back());
+    }
+    return tops;
+}
+
+std::string Rearrange1(std::istream &&is)
+{
+    auto stacks = ReadStacks(is);
+
+    std::string line;
     while (std::getline(is, line))
     {
         int count{}, from{}, to{};
@@ -51,22 +71,35 @@ std::string Rearrange(std::istream &&is)
         }
     }
 
-    std::string tops;
-    for (auto &s : stacks)
-    {
-        if (!s.empty())
-            tops.push_back(s.back());
-    }
-    return tops;
+    return GetTops(stacks);
 }
 
+std::string Rearrange2(std::istream &&is)
+{
+    auto stacks = ReadStacks(is);
+
+    std::string line;
+    while (std::getline(is, line))
+    {
+        int count{}, from{}, to{};
+        sscanf(line.c_str(), "move %d from %d to %d", &count, &from, &to);
+        auto &sto = stacks[to - 1];
+        auto &sfrom = stacks[from - 1];
+        sto += sfrom.substr(sfrom.size() - count);
+        sfrom.resize(sfrom.size() - count);
+    }
+
+    return GetTops(stacks);
+}
 using namespace boost::ut;
 
 suite s = [] {
     "2022-05"_test = [] {
-        expect("CMZ" == Rearrange(std::istringstream{TEST}));
+        expect("CMZ" == Rearrange1(std::istringstream{TEST}));
+        expect("MCD" == Rearrange2(std::istringstream{TEST}));
 
-        Printer::Print(__FILE__, "1", Rearrange(std::ifstream{INPUT}));
+        Printer::Print(__FILE__, "1", Rearrange1(std::ifstream{INPUT}));
+        Printer::Print(__FILE__, "2", Rearrange2(std::ifstream{INPUT}));
     };
 };
 
