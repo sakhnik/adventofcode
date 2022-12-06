839d94743f7a1548107b30b607c33d0a417b2379
Anatolii Sakhnik <sakhnik@gmail.com>
2022-12-06 10:32:16 +0200

    2022-06.2

--- 2022/06.cpp
+++ 2022/06.cpp
@@ -3,19 +3,19 @@
 
 namespace {
 
-int FindStartOfPacket(const std::string &message)
+int FindStartOfPacket(int length, const std::string &message)
 {
     std::unordered_map<char, int> counts;
 
     for (int i = 0; i < message.size(); ++i)
     {
         ++counts[message[i]];
-        if (i >= 4)
+        if (i >= length)
         {
-            if (!(--counts[message[i - 4]]))
-                counts.erase(message[i - 4]);
+            if (!(--counts[message[i - length]]))
+                counts.erase(message[i - length]);
         }
-        if (counts.size() == 4)
+        if (counts.size() == length)
             return i + 1;
     }
     return -1;
@@ -25,14 +25,21 @@ using namespace boost::ut;
 
 suite s = [] {
     "2022-06"_test = [] {
-        expect(5_i == FindStartOfPacket("bvwbjplbgvbhsrlpgdmjqwftvncz"));
-        expect(6_i == FindStartOfPacket("nppdvjthqldpwncqszvftbrmjlhg"));
-        expect(10_i == FindStartOfPacket("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-        expect(11_i == FindStartOfPacket("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+        expect(5_i == FindStartOfPacket(4, "bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        expect(6_i == FindStartOfPacket(4, "nppdvjthqldpwncqszvftbrmjlhg"));
+        expect(10_i == FindStartOfPacket(4, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        expect(11_i == FindStartOfPacket(4, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+
+        expect(19_i == FindStartOfPacket(14, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        expect(23_i == FindStartOfPacket(14, "bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        expect(23_i == FindStartOfPacket(14, "nppdvjthqldpwncqszvftbrmjlhg"));
+        expect(29_i == FindStartOfPacket(14, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        expect(26_i == FindStartOfPacket(14, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
 
         std::string input;
         std::getline(std::ifstream{INPUT}, input);
-        Printer::Print(__FILE__, "1", FindStartOfPacket(input));
+        Printer::Print(__FILE__, "1", FindStartOfPacket(4, input));
+        Printer::Print(__FILE__, "2", FindStartOfPacket(14, input));
     };
 };
 
