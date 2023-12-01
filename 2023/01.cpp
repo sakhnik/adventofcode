b91f5cc5e4db42379dbd7927985c4188739b5335
Anatolii Sakhnik <sakhnik@gmail.com>
2023-12-01 10:42:15 +0200

    2023-01.2

--- 2023/01.cpp
+++ 2023/01.cpp
@@ -3,15 +3,19 @@
 
 namespace {
 
-const char *const TEST = R"(1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-)";
-
 using namespace boost::ut;
 
-int Calibrate(std::istream &&is)
+const char *const DIGITS[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+int GetDigit(std::string_view s)
+{
+    if (isdigit(s[0]))
+        return s[0] - '0';
+    auto it = std::find_if(std::begin(DIGITS), std::end(DIGITS), [s](std::string_view d) { return s.starts_with(d); });
+    return it - std::begin(DIGITS);
+}
+
+int Calibrate1(std::istream &&is)
 {
     int sum{};
 
@@ -25,12 +29,56 @@ int Calibrate(std::istream &&is)
     return sum;
 }
 
+int Calibrate2(std::istream &&is)
+{
+    int sum{};
+
+    std::string line;
+    while (getline(is, line))
+    {
+        int a{}, b{};
+        for (int i = 0; i < line.size(); ++i)
+        {
+            std::string_view head{line.c_str() + i};
+            if ((a = GetDigit(head)) < 10)
+                break;
+        }
+
+        for (int i = line.size() - 1; i >= 0; --i)
+        {
+            std::string_view tail{line.c_str() + i};
+            if ((b = GetDigit(tail)) < 10)
+                break;
+        }
+
+        assert(a <= 9 && b <= 9);
+        sum += 10 * a + b;
+    }
+
+    return sum;
+}
+
 suite s = [] {
     "2023-01"_test = [] {
-        expect(142_i == Calibrate(std::istringstream{TEST}));
+        const char *const TEST1 = R"(1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+)";
+        expect(142_i == Calibrate1(std::istringstream{TEST1}));
 
-        std::ifstream ifs{INPUT};
-        Printer::Print(__FILE__, "1", Calibrate(std::ifstream{INPUT}));
+        const char *const TEST2 = R"(two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+)";
+        expect(281_i == Calibrate2(std::istringstream{TEST2}));
+
+        Printer::Print(__FILE__, "1", Calibrate1(std::ifstream{INPUT}));
+        Printer::Print(__FILE__, "2", Calibrate2(std::ifstream{INPUT}));
     };
 };
 
