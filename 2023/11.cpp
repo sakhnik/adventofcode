70ad86bcf460fbde44b266ba35213bd3dc2266dc
Anatolii Sakhnik <sakhnik@gmail.com>
2023-12-11 10:17:41 +0200

    2023-11.2

--- 2023/11.cpp
+++ 2023/11.cpp
@@ -5,11 +5,14 @@ namespace {
 
 using namespace boost::ut;
 
+using NumT = int64_t;
+using VecT = std::vector<NumT>;
+
 struct Expansion
 {
-    std::vector<int> coords, mapping;
+    VecT coords, mapping;
 
-    Expansion(const std::vector<int> &c)
+    Expansion(NumT mult, const VecT &c)
         : coords{std::move(c)}
     {
         std::sort(coords.begin(), coords.end());
@@ -20,11 +23,11 @@ struct Expansion
             if (d <= 1)
                 mapping.push_back(mapping.back() + d);
             else
-                mapping.push_back(mapping.back() + d + d - 1);
+                mapping.push_back(mapping.back() + (d - 1) * mult + 1);
         }
     }
 
-    int Get(int coord) const
+    NumT Get(NumT coord) const
     {
         auto idx = std::lower_bound(coords.begin(), coords.end(), coord) - coords.begin();
         return mapping[idx];
@@ -50,17 +53,22 @@ public:
         }
     }
 
-    int Task1() const
+    NumT Task1() const
     {
-        Expansion er{rows}, ec{cols};
-        std::vector<int> expanded_rows(rows.size()), expanded_cols(cols.size());
+        return Impl(2);
+    }
+
+    NumT Impl(NumT mult) const
+    {
+        Expansion er{mult, rows}, ec{mult, cols};
+        VecT expanded_rows(rows.size()), expanded_cols(cols.size());
         for (int i = 0; i < rows.size(); ++i)
         {
             expanded_rows[i] = er.Get(rows[i]);
             expanded_cols[i] = ec.Get(cols[i]);
         }
 
-        int sum{};
+        NumT sum{};
         for (int i = 1; i < rows.size(); ++i)
             for (int j = 0; j < i; ++j)
                 sum += std::abs(expanded_rows[i] - expanded_rows[j]) + std::abs(expanded_cols[i] - expanded_cols[j]);
@@ -68,7 +76,7 @@ public:
     }
 
 private:
-    std::vector<int> rows, cols;
+    VecT rows, cols;
 };
 
 suite s = [] {
@@ -86,11 +94,12 @@ suite s = [] {
 )";
         Cosmos test1{std::istringstream{TEST1}};
         expect(374_i == test1.Task1());
-        //expect(1_i == test1.Task2());
+        expect(1030_i == test1.Impl(10));
+        expect(8410_i == test1.Impl(100));
 
         Cosmos cosmos{std::ifstream{INPUT}};
         Printer::Print(__FILE__, "1", cosmos.Task1());
-        //Printer::Print(__FILE__, "2", pipe.Task2());
+        Printer::Print(__FILE__, "2", cosmos.Impl(1000000));
     };
 };
 
