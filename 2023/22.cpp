44775742e751be71bf098185b49571aa923e674c
Anatolii Sakhnik <sakhnik@gmail.com>
2023-12-22 12:42:20 +0200

    2023-22.2

--- 2023/22.cpp
+++ 2023/22.cpp
@@ -53,7 +53,7 @@ struct Bricks
                 --brick[1][2];
                 for (int j = i - 1; j >= 0; --j)
                 {
-                    if (AreOverlaping(bricks[j], brick))
+                    if (AreOverlapping(bricks[j], brick))
                     {
                         settled = true;
                         break;
@@ -79,12 +79,19 @@ struct Bricks
         return b0 <= a1;
     }
 
-    static bool AreOverlaping(const BrickT &a, const BrickT &b)
+    static bool AreOverlapping(const BrickT &a, const BrickT &b)
     {
         for (auto i : {0, 1, 2})
             if (!Intersect(a[0][i], a[1][i], b[0][i], b[1][i]))
                 return false;
         return true;
+    }
+
+    static bool IsSupportedBy(const BrickT &support, BrickT brick)
+    {
+        --brick[0][2];
+        --brick[1][2];
+        return AreOverlapping(support, brick);
     }
 
     int Task1()
@@ -94,45 +101,107 @@ struct Bricks
         for (int i = 0; i < bricks.size(); ++i)
         {
             // Try to take out this brick
-            auto brick = bricks[i];
-            auto top = brick[1][2];
-            // Potential dependencies
-            auto it = bottoms.find(top + 1);
-            if (it == bottoms.end())
-            {
-                // Nothing to support
-                ++sum;
-                continue;
-            }
-            // Other potential supporters
-            auto it2 = tops.find(top);
-            assert(it2 != tops.end());
-            if (IsStableWithout(i, it2->second, it->second))
+            if (IsStableWithout(i))
                 ++sum;
         }
         return sum;
     }
 
-    int64_t Task2()
+    int Task2()
     {
-        return 0;
+        int sum{};
+        for (int i = 0; i < bricks.size(); ++i)
+        {
+            sum += Crumble(i);
+        }
+        return sum;
     }
 
-    bool IsStableWithout(int i, const std::vector<int> &supports, const std::vector<int> &above) const
+    int Crumble(int i)
     {
+        std::unordered_set<int> to_disintegrate = {i};
+        auto zorder = [this](int a, int b) {
+            return bricks[a][0][2] > bricks[b][0][2];
+        };
+        std::priority_queue<int, std::vector<int>, decltype(zorder)> q(zorder);
+        q.push(i);
+        std::unordered_set<int> considered;
+        while (!q.empty())
+        {
+            int j = q.top();
+            q.pop();
+            if (j != i && IsSupported(j, to_disintegrate))
+                continue;
+            to_disintegrate.insert(j);
+            const auto &brick = bricks[j];
+            auto top = brick[1][2];
+            // Potential dependencies
+            auto it = bottoms.find(top + 1);
+            if (it != bottoms.end())
+            {
+                for (auto k : it->second)
+                {
+                    if (!considered.count(k) && IsSupportedBy(brick, bricks[k]))
+                    {
+                        q.push(k);
+                        considered.insert(k);
+                    }
+                }
+            }
+        }
+        return to_disintegrate.size() - 1;
+    }
+
+    bool IsSupported(int i, std::unordered_set<int> to_ignore)
+    {
+        auto brick = bricks[i];
+        --brick[0][2];
+        --brick[1][2];
+        auto bottom = brick[0][2];
+        if (bottom < 0)
+            return true;
+        // Potential supporters
+        auto it = tops.find(bottom);
+        if (it == tops.end())
+            return false;
+        for (auto j : it->second)
+        {
+            if (to_ignore.count(j))
+                continue;
+            if (AreOverlapping(brick, bricks[j]))
+                return true;
+        }
+        return false;
+    }
+
+    bool IsStableWithout(int i, std::unordered_set<int> to_ignore = {}) const
+    {
+        const auto &brick = bricks[i];
+        auto top = brick[1][2];
+        // Potential dependencies
+        auto it = bottoms.find(top + 1);
+        if (it == bottoms.end())
+            return true; // Nothing to support
+
+        // Other potential supporters
+        auto it2 = tops.find(top);
+        assert(it2 != tops.end());
+
         // Consider all the boxes above
-        for (auto j : above)
+        for (auto j : it->second)
         {
             auto higher_brick = bricks[j];
+            if (!IsSupportedBy(brick, higher_brick))
+                continue;
             --higher_brick[0][2];
             --higher_brick[1][2];
             bool support_found = false;
-            for (auto k : supports)
+            for (auto k : it2->second)
             {
-                if (k == i)
+                if (k == i || to_ignore.count(k))
                     continue;  // Ignore the brick in question
                 auto lower_brick = bricks[k];
-                if (AreOverlaping(lower_brick, higher_brick))
+                if (AreOverlapping(lower_brick, higher_brick))
                 {
                     support_found = true;
                     break;
@@ -168,10 +237,11 @@ suite s = [] {
 )";
         Bricks test1{std::istringstream{TEST1}};
         expect(5_i == test1.Task1());
+        expect(7_i == test1.Task2());
 
         Bricks task{std::ifstream{INPUT}};
         Printer::Print(__FILE__, "1", task.Task1());
-        //Printer::Print(__FILE__, "2", task.Task2());
+        Printer::Print(__FILE__, "2", task.Task2());
     };
 };
 
