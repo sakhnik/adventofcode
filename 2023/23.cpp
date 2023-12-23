1692a39e9639df2f5b5080ce2eaed0606ce0fb05
Anatolii Sakhnik <sakhnik@gmail.com>
2023-12-23 08:43:41 +0200

    2023-23.2

--- 2023/23.cpp
+++ 2023/23.cpp
@@ -16,7 +16,7 @@ struct Hiking
             map.push_back(line);
     }
 
-    int Task1()
+    int Walk(bool ignore_slopes)
     {
         using PosT = std::array<int, 2>;
 
@@ -32,6 +32,8 @@ struct Hiking
         int longest_walk{0};
 
         auto getPlace = [&](const PosT &pos) -> char {
+            if (pos[0] < 0 || pos[0] >= map.size()) return '#';
+            if (pos[1] < 0 || pos[1] >= map[0].size()) return '#';
             return map[pos[0]][pos[1]];
         };
 
@@ -39,51 +41,89 @@ struct Hiking
             return { pos[0] + dp[dir][0], pos[1] + dp[dir][1]};
         };
 
-        auto getDirections = [&](const PosT &pos, int dir) -> std::string {
-            int opposite_dir = (dir + 2) % 4;
+        auto getAllDirections = [&](const PosT &pos) -> std::string {
             char cur_place = getPlace(pos);
-            auto forced_dir = std::string{">v<^"}.find_first_of(cur_place);
-            if (forced_dir != std::string::npos)
-                return opposite_dir == forced_dir ? "" : std::string{char(forced_dir)};
+            if (!ignore_slopes)
+            {
+                auto forced_dir = std::string{">v<^"}.find_first_of(cur_place);
+                if (forced_dir != std::string::npos)
+                    return std::string{char(forced_dir)};
+            }
             std::string res;
             for (int i : {0, 1, 2, 3})
-                if (i != opposite_dir && getPlace(follow(pos, i)) != '#')
+                if (getPlace(follow(pos, i)) != '#')
                     res.push_back(i);
             return res;
         };
 
-        auto trace = [&](PosT pos, int dir, int dist, auto &&trace) {
+        using NeighboursT = std::unordered_map<PosT, int, decltype(posHash)>;
+        std::unordered_map<PosT, NeighboursT, decltype(posHash)> graph;
+        for (int row = 0; row < map.size(); ++row)
+        {
+            for (int col = 0; col < map.size(); ++col)
+            {
+                if (map[row][col] == '#')
+                    continue;
+                auto dirs = getAllDirections({row, col});
+                for (auto dir : dirs)
+                    graph[{row, col}][follow({row, col}, dir)] = 1;
+            }
+        }
+
+        for (auto it = graph.begin(); it != graph.end(); )
+        {
+            auto &neighbours = it->second;
+            if (neighbours.size() == 2)
+            {
+                auto it2 = neighbours.begin();
+                auto left = *it2++;
+                auto right = *it2++;
+                auto &left_neighbour = graph[left.first];
+                auto &right_neighbour = graph[right.first];
+                left_neighbour.erase(it->first);
+                right_neighbour.erase(it->first);
+                auto it3 = left_neighbour.find(right.first);
+                int dist = it3 != left_neighbour.end() ? it3->second : 0;
+                dist = std::max(dist, neighbours[left.first] + neighbours[right.first]);
+                left_neighbour[right.first] = dist;
+                right_neighbour[left.first] = dist;
+                it = graph.erase(it);
+            }
+            else
+                ++it;
+        }
+
+        //std::cerr << graph.size() << std::endl;
+
+        auto trace = [&](PosT pos, int dist, auto &&trace) {
             if (pos == target)
             {
                 longest_walk = std::max(longest_walk, dist);
+                //std::cerr << longest_walk << std::endl;
                 return;
             }
-            auto directions = getDirections(pos, dir);
-            if (directions.empty())
-                return;
-            if (directions.size() == 1)
-            {
-                trace(follow(pos, directions[0]), directions[0], dist + 1, trace);
-                return;
-            }
-
             if (visited.count(pos))
                 return;
             visited.insert(pos);
 
-            for (auto next_dir : directions)
-                trace(follow(pos, next_dir), next_dir, dist + 1, trace);
+            for (auto next_dir : graph[pos])
+                trace(next_dir.first, dist + next_dir.second, trace);
 
             visited.erase(pos);
         };
-        trace(start, 1, 0, trace);
+        trace(start, 0, trace);
 
         return longest_walk;
     }
 
+    int Task1()
+    {
+        return Walk(false);
+    }
+
     int Task2()
     {
-        return 0;
+        return Walk(true);
     }
 };
 
@@ -115,11 +155,11 @@ suite s = [] {
 )";
         Hiking test1{std::istringstream{TEST1}};
         expect(94_i == test1.Task1());
-        //expect(7_i == test1.Task2());
+        expect(154_i == test1.Task2());
 
         Hiking task{std::ifstream{INPUT}};
         Printer::Print(__FILE__, "1", task.Task1());
-        //Printer::Print(__FILE__, "2", task.Task2());
+        Printer::Print(__FILE__, "2", task.Task2());
     };
 };
 
