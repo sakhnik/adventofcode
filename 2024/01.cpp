439b6ff59114a04a3ebc9185b6cf89b1552d7b6e
Anatolii Sakhnik <sakhnik@gmail.com>
2024-11-30 18:24:40 +0200

    Prepare for 2024-01

---
+++ 2024/01.cpp
@@ -0,0 +1,31 @@
+#include "../test.hpp"
+
+namespace {
+
+using namespace boost::ut;
+
+suite s = [] {
+    "01"_test = [] {
+//        const char *const TEST1 = R"(1abc2
+//pqr3stu8vwx
+//a1b2c3d4e5f
+//treb7uchet
+//)";
+//        expect(142_i == Calibrate1(std::istringstream{TEST1}));
+//
+//        const char *const TEST2 = R"(two1nine
+//eightwothree
+//abcone2threexyz
+//xtwone3four
+//4nineeightseven2
+//zoneight234
+//7pqrstsixteen
+//)";
+//        expect(281_i == Calibrate2(std::istringstream{TEST2}));
+//
+//        Printer::Print(__FILE__, "1", Calibrate1(std::ifstream{INPUT}));
+//        Printer::Print(__FILE__, "2", Calibrate2(std::ifstream{INPUT}));
+    };
+};
+
+} //namespace;
