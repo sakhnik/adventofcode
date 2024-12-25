c64cb7330fada2d91f9685cd37494e2f337e182c
Anatolii Sakhnik <sakhnik@gmail.com>
2024-12-25 09:26:00 +0200

    Simplify 2024-25

--- 2024/25.cpp
+++ 2024/25.cpp
@@ -6,61 +6,35 @@ using namespace boost::ut;
 
 std::pair<int, int> Task(std::istream &&is)
 {
-    using CodeT = std::vector<int>;
-    std::vector<CodeT> locks, keys;
+    using PatT = std::vector<std::string>;
+    std::vector<PatT> patterns;
 
-    std::vector<std::string> pat;
-    auto processPat = [&]() {
-        if (pat[0] == "#####") {
-            // lock
-            CodeT code;
-            for (int c = 0; c < 5; ++c) {
-                for (int h = 1; h <= 6; ++h) {
-                    if (pat[h][c] != '#') {
-                        code.push_back(h - 1);
-                        break;
-                    }
-                }
-            }
-            locks.push_back(code);
-        } else {
-            // key
-            CodeT code;
-            for (int c = 0; c < 5; ++c) {
-                for (int h = 5; h >= 0; --h) {
-                    if (pat[h][c] != '#') {
-                        code.push_back(5 - h);
-                        break;
-                    }
-                }
-            }
-            keys.push_back(code);
-        }
-        pat.clear();
-    };
-
+    PatT pat;
     std::string line;
     while (getline(is, line)) {
         if (!line.empty()) {
             pat.push_back(line);
             continue;
         }
-        processPat();
+        patterns.push_back(pat);
+        pat.clear();
     }
-    processPat();
+    patterns.push_back(pat);
 
-    auto fit = [](const CodeT &key, const CodeT &lock) {
-        for (int i = 0; i < 5; ++i) {
-            if (key[i] + lock[i] > 5)
-                return false;
+    auto fit = [](const PatT &a, const PatT &b) {
+        for (int i = 0; i < a.size(); ++i) {
+            for (int j = 0; j < a[0].size(); ++j) {
+                if (a[i][j] == '#' && b[i][j] == '#')
+                    return false;
+            }
         }
         return true;
     };
 
     int task1{};
-    for (auto lock : locks) {
-        for (auto key : keys) {
-            if (fit(key, lock))
+    for (int i = 1; i < patterns.size(); ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (fit(patterns[i], patterns[j]))
                 ++task1;
         }
     }
